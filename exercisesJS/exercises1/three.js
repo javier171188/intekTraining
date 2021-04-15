c9ff297f4a8a7fe7c8dc6cd2a717d8d7ee5cb1bd
javier171188 <javier171188@hotmail.com>
2021-04-15 11:49:24 -0500

    s1e3 done

---
+++ exercisesJS/exercises1/three.js
@@ -0,0 +1,14 @@
+function setAtributes(className = 'title',color='blue',fontSize=10, bgColor='red'){
+    let obj = {element:document.getElementsByClassName(className)[0],
+                fun: function(){
+                        this.element.style.color = color;
+                        this.element.style.backgroundColor = bgColor;
+                        this.element.style.fontSize = String(fontSize) +  'px';
+                }
+
+    }
+    obj.fun()
+}
+
+
+setAtributes('exercise', 'blue', 20, 'black');
