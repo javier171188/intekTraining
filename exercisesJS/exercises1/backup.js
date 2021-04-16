535ef0e4e06b5206ddb1ce966fa7a2f7ac9e4199
javier171188 <javier171188@hotmail.com>
2021-04-16 16:30:51 -0500

    new version of tree

---
+++ exercisesJS/exercises1/backup.js
@@ -0,0 +1,53 @@
+let tree = {'A':{'a':{'aa': null},'b':{'ba':null, 'bb':null},'c':{'ca':null, 'cb':{'cab':null}}}};
+
+let canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
+ctx.font = "30px Arial";
+
+let branchesObj = {0:tree};
+let positions = {};
+let sep;
+let ypos = 50;
+let xpos = 0;
+let level = 0;
+let checker = true;
+
+while (checker){
+    sep = 1000/(Object.keys(branchesObj[level]).length+1)
+    branchesObj[level+1] = {};
+    for (let node in branchesObj[level]){
+        xpos += sep;
+        ctx.fillText(node, xpos, ypos);
+                
+        if(!branchesObj[level][node]) branchesObj[level][node] = {};
+        if (!positions[node]){
+            positions[node] = {};
+        } 
+        positions[node]['x'] = positions[node]['x_parent'] -sep/2 + xpos;
+        positions[node]['y'] = ypos;
+        
+        
+        if (positions[node]['y_parent']){
+            ctx.beginPath();
+            ctx.moveTo(positions[node]['x_parent']+10, positions[node]['y_parent']);
+            ctx.lineTo(xpos+10, ypos-20);
+            ctx.closePath();
+            ctx.stroke();
+        }
+        for (let subnode in branchesObj[level][node]){
+            branchesObj[level+1][subnode] = branchesObj[level][node][subnode];
+            positions[subnode] = {'x_parent':xpos, 'y_parent': ypos};
+            
+        }
+    }
+if (Object.keys(branchesObj[level+1]).length == 0) checker = false;
+level++;
+ypos+=110;
+xpos=0;
+}
+console.log(positions);
+
+
+
+
+
--- exercisesJS/exercises1/six.js
+++ exercisesJS/exercises1/six.js
@@ -1,53 +1,62 @@
-let tree = {'A':{'a':{'aa': null},'b':{'ba':null, 'bb':null},'c':{'ca':null, 'cb':{'cab':null}}}};
+let tree = {'A':{'a':{'aa': {}},'b':{'ba':{}, 'bb':{}},'c':{'ca':{}, 'cb':{'cab':{}}}}};
 
 let canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
 ctx.font = "30px Arial";
 
-let branchesObj = {0:tree};
-let positions = {};
-let sep;
-let ypos = 50;
-let xpos = 0;
 let level = 0;
-let checker = true;
+let numNodes = Object.keys(tree).length;
+let sep = 1000/(numNodes+1);
+let ypos = 50;
+let yspace = 110;
+let xpos = 0;
+let branchesObj = {0:{}};
+let positions = {};
+let subSep = 1/(numNodes+1);
 
-while (checker){
-    sep = 1000/(Object.keys(branchesObj[level]).length+1)
-    branchesObj[level+1] = {};
-    for (let node in branchesObj[level]){
-        xpos += sep;
-        ctx.fillText(node, xpos, ypos);
-        ctx.beginPath();
-        ctx.moveTo(0, 0);
-        ctx.lineTo(1000, 1000);
-        ctx.closePath();
-        
-        if(!branchesObj[level][node]) branchesObj[level][node] = {};
-        if (!positions[node]) positions[node] = {};
-        positions[node]['x'] = xpos;
-        positions[node]['y'] = ypos;
-        if (positions[node]['y_parent']){
-            ctx.beginPath();
-            ctx.moveTo(positions[node]['x_parent']+10, positions[node]['y_parent']);
-            ctx.lineTo(xpos+10, ypos-20);
-            ctx.closePath();
-            ctx.stroke();
-        }
-        for (let subnode in branchesObj[level][node]){
-            branchesObj[level+1][subnode] = branchesObj[level][node][subnode];
-            positions[subnode] = {'x_parent':xpos, 'y_parent': ypos};
-            
-        }
+//Place the roots.
+for (let r in tree){
+    xpos += sep;
+    ctx.fillText(r, xpos, ypos);
+    positions[r] = {'x': xpos, 'y':ypos};
+    let c = 0;
+    for (let node in tree[r]){
+        branchesObj[level][node] = tree[r][node];
+        positions[node] = {'x_parent':xpos, 'y_parent':ypos, 'x': xpos - 0.5*sep + subSep*sep*c, 'y': ypos+yspace};
+        positions[node]['space'] = sep;
+        c++;
     }
-if (Object.keys(branchesObj[level+1]).length == 0) checker = false;
-level++;
-ypos+=110;
-xpos=0;
 }
+ypos+=yspace;
+//Create the tree
+while(level < 5){ //  <-----remember to change
+    level++;
+    ypos+=yspace;
+    numNodes = Object.keys(branchesObj[level-1]).length;
+    let c=0;
+    branchesObj[level] = {};
+    for (let node in branchesObj[level-1]){
+        sep = positions[node]['space']/numNodes;
+        ctx.fillText(level, positions[node]['x'], positions[node]['y']);
+        for (let subNode in branchesObj[level-1][node]){
+            console.log(node, subNode);
+            branchesObj[level][subNode] = branchesObj[level-1][node][subNode];
+            positions[subNode] = {'x_parent':positions[node]['x']};
+            positions[subNode]['y_parent'] = positions[node]['y'];
+            positions[subNode]['y'] = ypos;
+            positions[subNode]['space'] = sep;
+            xpos = positions[subNode]['x_parent'] + positions[node]['space']*(c/(1+numNodes)-0.5);
+            positions[subNode]['x'] = xpos;
+            c++;
+        }
+        
+    }
+
+
+    
+    
+    console.log(level);
+}
+
+console.log(branchesObj);
 console.log(positions);
-
-
-
-
-
