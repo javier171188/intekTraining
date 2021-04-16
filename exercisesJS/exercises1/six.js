d58967b0d307285c51844d728865a12b74526891
javier171188 <javier171188@hotmail.com>
2021-04-15 19:49:11 -0500

    I am trying to  give the parent positions

--- exercisesJS/exercises1/six.js
+++ exercisesJS/exercises1/six.js
@@ -5,6 +5,7 @@ let ctx = canvas.getContext("2d");
 ctx.font = "30px Arial";
 
 let branchesObj = {0:tree};
+let positions = {};
 let sep;
 let ypos = 50;
 let xpos = 0;
@@ -17,16 +18,22 @@ while (checker){
     for (let node in branchesObj[level]){
         xpos += sep;
         ctx.fillText(node, xpos, ypos);
+        if(!branchesObj[level][node]) branchesObj[level][node] = {};
+        positions[node] = {};
+        positions[node]['x'] = xpos;
+        positions[node]['y'] = ypos;
         for (let subnode in branchesObj[level][node]){
             branchesObj[level+1][subnode] = branchesObj[level][node][subnode];
+            if(!positions[subnode]) positions[subnode] = {};
+            positions[subnode]['x_parent'] = positions[node]['x']
         }
     }
 if (Object.keys(branchesObj[level+1]).length == 0) checker = false;
 level++;
-ypos+=100;
+ypos+=110;
 xpos=0;
 }
-
+console.log(positions);
 
 
 
