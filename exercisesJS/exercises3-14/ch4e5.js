26e69788bc2532cee30bc840e7da589b3b9d52da
javier171188 <javier171188@hotmail.com>
2021-04-21 12:15:43 -0500

    ch4e5 done

--- exercisesJS/exercises3-14/ch4e5.js
+++ exercisesJS/exercises3-14/ch4e5.js
@@ -1,5 +1,36 @@
 let e = document.getElementsByClassName('exercise')[0];
 
-e.innerHTML = 'Ready to code';
+e.innerHTML = 'var x1 = 1, y1 = 2, z1 = 1; <br/>';
+e.innerHTML += 'var x2 = 2, y2 = 2, z2 = 4; <br/>';
 
-function distance(x,y,a,b,c,d);
+var x1 = 1, y1 = 2, z1 = 1;
+var x2 = 2, y2 = 2, z2 = 4;
+
+
+
+function distance(x,y,a,b,c,d){
+    let distance;
+    if (typeof d != 'number' && typeof c === 'number' ) {
+        throw 'Insufficient parameters';
+    }
+    if (typeof b != 'number') {
+        throw 'Insufficient parameters';
+    }
+    if (typeof c != 'number' && typeof d != 'number') {
+        distance = Math.sqrt((x-a)*(x-a) + (y-b)*(y-b));
+        return distance;
+    }
+    distance = Math.sqrt((x-b)*(x-b) + (y-c)*(y-c) +(a-d)*(a-d))
+    return distance;
+}
+var delta1 = distance (x1, y1, x2, y2); // delta = 1
+var delta2 = distance (x1, y1, z1, x2, y2, z2); // delta = 3.1622…
+e.innerHTML += '<br/> var delta1 = distance (x1, y1, x2, y2) <br/>';
+e.innerHTML += `delta1: ${delta1} <br/>`;
+
+e.innerHTML += '<br/> var delta2 = distance (x1, y1, z1, x2, y2, z2); <br/>';
+e.innerHTML += `delta2: ${delta2} <br/>`;
+
+console.log(delta1);
+console.log(delta2)
+distance (x1, x2);
