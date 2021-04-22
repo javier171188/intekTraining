ff02d27a5332a6daff6622e1830e1d19b8a6ca09
javier171188 <javier171188@hotmail.com>
2021-04-22 13:27:38 -0500

    s1 e2 corrected

--- exercisesJS/exercises1/two.js
+++ exercisesJS/exercises1/two.js
@@ -1,29 +1,29 @@
 function limitFunc(fn, num){
     let counter = 0;
-    function limiting(){
+    let r;
+    function limiting(...input){
         if (counter< num){
             counter++;
-            return fn();
+            r = fn(...input)
+            return r;
         }
-        return null;
+        throw 'This function has reached its limit of ejecutions';
     }
     return limiting;
 }
 
 e = document.getElementsByClassName("exercise")[0]
 
-function fn(){
-    e.innerHTML += 'I am being executed.';
-    e.innerHTML += '<br/>';
-    e.innerHTML += '<br/>';
+function sum(a,b){
+    return a+b;
 }
 
-var limited = limitFunc (fn, 2);
+var limited = limitFunc (sum, 2);
 e.innerHTML += 'Execution 1 <br/>'
-limited (); // executes fine
+e.innerHTML += `${(limited (1,1))} <br/>`; // executes fine
 e.innerHTML += 'Execution 2 <br/>'
-limited (); // executes fine
+e.innerHTML += `${(limited (1,2))} <br/>`; // executes fine
 e.innerHTML += 'Execution 3 <br/>'
-limited (); // does not execute
+e.innerHTML += `${(limited (1,3))} <br/>`; // does not execute
 e.innerHTML += 'Execution 4 <br/>'
-limited (); // does not execute
+e.innerHTML += `${(limited (1,4))} <br/>`;// does not execute
