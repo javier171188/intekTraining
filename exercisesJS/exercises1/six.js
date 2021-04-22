5acbcc0d3474c4e2694cf535dc346c81434a85cd
javier171188 <javier171188@hotmail.com>
2021-04-22 17:12:27 -0500

    s1 e6 corrected

--- exercisesJS/exercises1/six.js
+++ exercisesJS/exercises1/six.js
@@ -1,61 +1,19 @@
 let tree = {'A':{'a':{'aa': {}},'b':{'ba':{}, 'bb':{}},'c':{'ca':{}, 'cb':{'cab':{}}}}};
+let e = document.getElementsByClassName('exercise')[0];
+let branches = [tree];
+let nextBranches = [];
+e.innerHTML += 'The values are: <br/>';
 
-let canvas = document.querySelector("canvas");
-let ctx = canvas.getContext("2d");
-ctx.font = "30px Arial";
-
-let level = 0;
-let numNodes = Object.keys(tree).length;
-let sep = 1000/(numNodes+1);
-let ypos = 50;
-let yspace = 110;
-let xpos = 0;
-let branchesObj = {0:{}};
-let positions = {};
-let subSep = 1/(numNodes+1);
-
-//Place the roots.
-for (let r in tree){
-    xpos += sep;
-    ctx.fillText(r, xpos, ypos);
-    positions[r] = {'x': xpos, 'y':ypos};
-    let c = 0;
-    for (let node in tree[r]){
-        branchesObj[level][node] = tree[r][node];
-        positions[node] = {'x_parent':xpos, 'y_parent':ypos, 'x': xpos - 0.5*sep + subSep*sep*c, 'y': ypos+yspace};
-        positions[node]['space'] = sep;
-        c++;
-    }
-}
-ypos+=yspace;
-checker = true;
-ctx.font = "25px Arial";
-//Create the tree
-while(checker){ 
-    level++;
-    ypos+=yspace;
-    branchesObj[level] = {};
-    for (let node in branchesObj[level-1]){
-        numNodes = Object.keys(branchesObj[level-1][node]).length;
-        sep = positions[node]['space']/(numNodes+1);
-        ctx.fillText(node, positions[node]['x'], positions[node]['y']);
-        ctx.beginPath();
-        ctx.moveTo(positions[node]['x_parent']+10, positions[node]['y_parent']);
-        ctx.lineTo(positions[node]['x']+10, positions[node]['y']-20);
-        ctx.closePath();
-        ctx.stroke();
-        let c=1;
-        for (let subNode in branchesObj[level-1][node]){
-            branchesObj[level][subNode] = branchesObj[level-1][node][subNode];
-            positions[subNode] = {'x_parent':positions[node]['x']};
-            positions[subNode]['y_parent'] = positions[node]['y'];
-            positions[subNode]['y'] = ypos;
-            positions[subNode]['space'] = sep;
-            xpos = positions[subNode]['x_parent'] + positions[subNode]['space']*(c/(1+numNodes)-0.5);
-            positions[subNode]['x'] = xpos;
-            c++;
+while (branches.length !== 0){
+    for (b of branches){
+        for (nb in b){
+            console.log(nb);
+            e.innerHTML += nb + '<br/>';
+            if (Boolean(b[nb])){
+                nextBranches.push(b[nb]);
+            }
         }
-        
     }
-    if (Object.keys(branchesObj[level]).length ==0) checker = false;
+    branches = nextBranches.slice();
+    nextBranches =[];   
 }
