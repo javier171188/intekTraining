61711972939e50f052b2e9eff879b533af7f0203
javier171188 <javier171188@hotmail.com>
2021-04-26 16:22:29 -0500

    changed the name of variable

--- exercisesJS/exercises3-14/ch13e1.js
+++ exercisesJS/exercises3-14/ch13e1.js
@@ -5,8 +5,8 @@ for (let i=0; i<25; i++){
     e.innerHTML += `<div class ="element">${i}<div/>`;
 }
 
-e.addEventListener('click', function(e){
-    let inText = e.target.innerHTML;
+e.addEventListener('click', function(ev){
+    let inText = ev.target.innerHTML;
     inText = inText.replace('<div></div>','');
     console.log(inText);
     window.alert(inText);
