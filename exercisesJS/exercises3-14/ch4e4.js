46a7c355971d759b3b439a7e0ea9dbbc4ba5909b
javier171188 <javier171188@hotmail.com>
2021-04-30 17:28:14 -0500

    chapter4 exercise4 corrected

--- exercisesJS/exercises3-14/ch4e4.js
+++ exercisesJS/exercises3-14/ch4e4.js
@@ -1,11 +1,27 @@
 let e = document.getElementsByClassName('exercise')[0];
 
 
-
 function dataType(...elements){
+    let theType;
     for (element of elements){
-        console.log(element, typeof element);
-        e.innerHTML += `${element}: ${typeof element} <br/>`;
+        if (typeof element === 'number'){
+            if (Number.isInteger(element)){
+                theType = 'number';
+            }else{
+                theType = 'float';
+            }
+        }
+        else if (typeof element === 'object'){
+            if (Array.isArray(element)){
+                theType = 'array';
+            } else{
+                theType = 'object';
+            }
+        }else{
+            theType = typeof element;
+        }
+        console.log(element, theType);
+        e.innerHTML += `${element}: ${theType} <br/>`;
     }
 }
 
