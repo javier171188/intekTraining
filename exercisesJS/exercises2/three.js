15e146a7aa120b7ebef6be75200dad13b5fddd96
javier171188 <javier171188@hotmail.com>
2021-04-30 16:21:17 -0500

    exercise 3, set 2 corrected

--- exercisesJS/exercises2/three.js
+++ exercisesJS/exercises2/three.js
@@ -2,19 +2,24 @@ let e = document.getElementsByClassName('exercisea')[0]
 e.innerHTML += 'Input: aBc, <br/>';
 
 function cycle(s){
-    let dict = {'a':'b', 'b':'c' , 'c':'d' , 'd':'e' , 'e':'f' , 'f':'g' , 'g':'h',
-     'h':'i' , 'i':'j' , 'j':'k' , 'k':'l', 'l': 'm', 'm':'n' , 'n':'o' , 'o':'p' , 
-     'p':'q' , 'q':'r' , 'r':'s' , 's':'t' , 't':'u' , 'u':'v' , 'v':'w', 'w':'x',
-     'x':'y',   'y':'z', 'z':'a', '1':'2','2':'3','3':'4','4':'5','5':'6','6':'7',
-     '7':'8','8':'9','9':'0','0':'1', 'A':'B','B':'C','C':'D','D':'E','E':'F','F':'G',
-     'G':'G','H':'I','I':'J', 'J':'K','K':'L', 'L':'M','M':'N','N':'O','O':'P','P':'Q',
-     'Q':'R', 'R':'S','S':'T' ,'T':'U','U':'V','V':'W','W':'X','X':'Y','Y':'Z','Z':'A' }
-    let newS = '' 
-    for (l of s){
-        newS += dict[l];
+    function nextLetter(l){
+        let code = l.charCodeAt(0);
+        if (code===122){
+            code = 97;
+        } else if (code === 90){
+            code = 65;
+        } else if(code === 57){
+            code = 48;
+        } else{
+            code++;
+        }
+        l = String.fromCharCode(code);
+        return l;
     }
-    return newS;
+    s = s.replace(/[A-Za-z0-9]/g,nextLetter);
+    return s;
 }
+
 
 e.innerHTML += `Output: ${cycle('aBc')}.`;
 e.innerHTML += '<br/> <br/>';
