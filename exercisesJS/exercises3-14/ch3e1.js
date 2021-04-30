83dcf3362ebcd3b4c5a1ff2f71306511830ea941
javier171188 <javier171188@hotmail.com>
2021-04-30 16:34:45 -0500

    chapter 3 exercise 1 corrected

--- exercisesJS/exercises3-14/ch3e1.js
+++ exercisesJS/exercises3-14/ch3e1.js
@@ -5,36 +5,10 @@ let b = 6;
 
 e.innerHTML += a + ', ' + b;
 
-function toBaseT(n){
-    let digits = [];
-    let r;
-    let baseT = '';
-    while(n>0){
-        r = n%13;
-        if (r==12){
-            digits.push('C');
-        } else if (r==11){
-            digits.push('B');
-        }else if (r==10){
-            digits.push('A')
-        }else{
-            digits.push(r);
-        }
-
-        
-        n = Math.floor(n/13);
-    }
-        
-    for (let l in digits){
-        baseT += String(digits[digits.length-l-1]);
-    }
-    return(baseT);
-
-}
 
 function mul(x1,x2){
     let r = x1*x2;
-    return toBaseT(r);
+    return r.toString(13);
 }
 
 e.innerHTML += '<br/>'
