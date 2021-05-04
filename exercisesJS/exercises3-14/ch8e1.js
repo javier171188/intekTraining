0699336a68e5513018ad828ec69e7aa414df4e9a
javier171188 <javier171188@hotmail.com>
2021-05-04 13:13:44 -0500

    chapter 8 exercise 1 corrected

--- exercisesJS/exercises3-14/ch8e1.js
+++ exercisesJS/exercises3-14/ch8e1.js
@@ -2,15 +2,21 @@ let e = document.getElementsByClassName('exercise')[0];
 
 
 
-let randomSentences = ['random sentence 1', 'random sentence 2'];
+let nouns = ['Raul ', 'Mark ', 'Diana ','Mary '];
+let verbs = ['walks to ', 'looks at ', 'is in ', 'likes '];
+let articles = ['a ', 'the '];
+let places = ['park', 'store', 'theatre', 'living room'];
+
+
 
 function displaySentence(){
     function choose(choices) {
         var index = Math.floor(Math.random() * choices.length);
         return choices[index];
       }
-      console.log(choose(randomSentences));
-      e.innerHTML = choose(randomSentences);
+      let sentence = choose(nouns)+choose(verbs)+choose(articles)+choose(places);
+      console.log(sentence);
+      e.innerHTML = sentence;
 }
 
 setInterval(displaySentence, 60000);
