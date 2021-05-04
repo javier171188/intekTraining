d64927eae779e4b0ff0691245fb2487d60910354
javier171188 <javier171188@hotmail.com>
2021-05-04 14:43:53 -0500

    chapter 13 exercise1 corrected

--- exercisesJS/exercises3-14/ch13e1.js
+++ exercisesJS/exercises3-14/ch13e1.js
@@ -1,9 +1,14 @@
-let e = document.getElementsByClassName('exercise')[0];
+let e = document.querySelector('.exercise');
 
 
+var fragment = new DocumentFragment()
 for (let i=0; i<25; i++){
-    e.innerHTML += `<div class ="element">${i}<div/>`;
+    var div = document.createElement('div');
+    div.setAttribute('class', "element");
+    div.textContent = i;
+    fragment.appendChild(div);
 }
+e.appendChild(fragment);
 
 e.addEventListener('click', function(ev){
     let inText = ev.target.innerHTML;
