ca9744c550573d47f52581e4edc1c0de725a2a0e
javier171188 <javier171188@hotmail.com>
2021-05-06 19:47:40 -0500

    Chapter 15 exercise 1 seems to be working, I will test it in three browsers

--- exercisesJS/exercises8-15/exercise1.js
+++ exercisesJS/exercises8-15/exercise1.js
@@ -12,18 +12,21 @@ let lastIndex = indexes[indexes.length-1];
 
 placeNotes();
 button.addEventListener('click',saveCurrentNote);
+textSpace.addEventListener('keydown', allowTabs);
 
 //functions
 function saveCurrentNote(){
     if (Boolean(textSpace.value)){
         let newIndex = Number(lastIndex) +1;
-        localStorage.setItem(newIndex, textSpace.value);
+        let d = new Date();
+        let noteData = {'creaDate':d.toString(), 'lastMDate':d.toString(), "note":textSpace.value};
+        let strData = JSON.stringify(noteData);
+        localStorage.setItem(newIndex, strData);
         textSpace.value = '';
         let renewIndexes = localStorage.getItem(0) + ' '  + String(newIndex);
         localStorage.setItem(0, renewIndexes);
         indexes = localStorage.getItem(0).split(' ')
         lastIndex = indexes[indexes.length-1];
-        
         placeNotes();
     }
 }
@@ -44,12 +47,15 @@ function placeNotes(){
             editB.setAttribute('dbid',i);
             let viewB = buttons.querySelector('.vbutton');
             viewB.setAttribute('dbid',i);
-            p.textContent = localStorage.getItem(i).slice(0,10);
-            if (localStorage.getItem(i).length > 10){
+            let noteData = localStorage.getItem(i);
+            let parsedData = JSON.parse(noteData);
+            p.textContent = parsedData['note'].slice(0,10);
+            if (parsedData['note'].length > 10){
                 p.textContent +=  '...';
             }
             let a = document.importNode(div, true);
             fragment.appendChild(a);
+
         }
     }
     pastNotes.appendChild(fragment);
@@ -72,33 +78,49 @@ function editNote(event){
     let editButton = document.querySelector('.editingbutton');
     let saveButton = document.querySelector('.savingbutton');
     let activity = document.querySelector('.activity');
+    let parsedData = JSON.parse(text);
+    let creation = document.querySelector('.creation');
+    let modified = document.querySelector('.modified');
+    let dates = document.querySelector(".hiddates");
     editButton.style.display = "inline";
     saveButton.style.display = 'none';
     pastNotes.style.display = "none";
     activity.textContent = "Edit this note";
-    textSpace.value = text;
+    textSpace.value = parsedData['note'];
     textSpace.setAttribute('placeholder', 'Write a new version for the note');
+    creation.textContent = `Creation date: ${parsedData['creaDate']}.`;
+    modified.textContent = `Last modification: ${parsedData['lastMDate']}`;
+    dates.style.display = 'flex';
     editButton.addEventListener('click', ()=>{
         editButton.style.display = "none";
         saveButton.style.display = 'inline';
         pastNotes.style.display = 'block';
         if (Boolean(textSpace.value)){
-            localStorage.setItem(dbid, textSpace.value);
+            let d = new Date;
+            parsedData['note'] = textSpace.value;
+            parsedData['lastMDate'] = d.toString()
+            let strData = JSON.stringify(parsedData);
+            localStorage.setItem(dbid, strData);
         }
         textSpace.value = '';
         activity.textContent = 'Create a Note';
         textSpace.setAttribute('placeholder', 'Write a note here');
+        dates.style.display = 'none';
         placeNotes();
         }, {once : true});
 }
 
 function viewNote(event){
     let dbid = event.getAttribute('dbid');
-    let text = localStorage.getItem(dbid);
     let editButton = document.querySelector('.editingbutton');
     let saveButton = document.querySelector('.savingbutton');
     let activity = document.querySelector('.activity');
     let dates = document.querySelector(".hiddates");
+    let noteData = localStorage.getItem(dbid);
+    let parsedData = JSON.parse(noteData);
+    let text = parsedData['note'];
+    let creation = document.querySelector('.creation');
+    let modified = document.querySelector('.modified');
     editButton.style.display = "inline";
     saveButton.style.display = 'none';
     pastNotes.style.display = "none";
@@ -106,7 +128,9 @@ function viewNote(event){
     textSpace.value = text;
     textSpace.readOnly = true;
     editButton.textContent = 'Go back';
-    dates.style.display = 'block';
+    dates.style.display = 'flex';
+    creation.textContent = `Creation date: ${parsedData['creaDate']}.`;
+    modified.textContent = `Last modification: ${parsedData['lastMDate']}`;
     editButton.addEventListener('click', ()=>{
         editButton.style.display = "none";
         editButton.textContent = 'Edit';
@@ -119,4 +143,17 @@ function viewNote(event){
         dates.style.display = 'none';
         placeNotes();
         }, {once : true});
+}
+
+function allowTabs(event){  
+    //Source: https://stackoverflow.com/questions/6637341/use-tab-to-indent-in-textarea
+    if (event.key == 'Tab') {
+        event.preventDefault();
+        let start = this.selectionStart;
+        let end = this.selectionEnd;
+        this.value = this.value.slice(0, start) +  "\t" + this.value.slice(end);
+
+        //To place the cursor in the right character
+        this.selectionEnd = start + 1;
+    }
 }
