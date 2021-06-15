0baba73ab1d66c1505639eab8eb0d1387a0e056d
javier171188 <javier171188@hotmail.com>
2021-06-15 17:34:39 -0500

    view is now a sigleton

--- patterns/exercise1-IEH.js
+++ patterns/exercise1-IEH.js
@@ -322,241 +322,251 @@ const presenter = (function(pubsub) {
 
 // View
 const view = (function (pubsub) {
-    const activityTitle = document.querySelector('.activity')
-    const datesBox = document.querySelector('.hiddates')
-    const textSpace = document.getElementsByName('textarea')[0]
-    const saveButton = document.querySelector('.savingbutton')
-    const editButton = document.querySelector('.editingbutton')
-    const cancelButton = document.querySelector('.cancelingbutton')
-    const pastNotes = document.querySelector('.pastnotes')
-    const creationDP = document.querySelector('.creation')
-    const lastMDP = document.querySelector('.modified')
-    const undoButton = document.querySelector('.undobutton')
-    const searchBox = document.querySelector('#searchingbox')
-    let draggedNote
+    var viewInstance
 
-    function notifyChangeBox() {
-        const filter = searchBox.value
-        pubsub.publish('newText', filter)
-    };
+    function init() {
+        const activityTitle = document.querySelector('.activity')
+        const datesBox = document.querySelector('.hiddates')
+        const textSpace = document.getElementsByName('textarea')[0]
+        const saveButton = document.querySelector('.savingbutton')
+        const editButton = document.querySelector('.editingbutton')
+        const cancelButton = document.querySelector('.cancelingbutton')
+        const pastNotes = document.querySelector('.pastnotes')
+        const creationDP = document.querySelector('.creation')
+        const lastMDP = document.querySelector('.modified')
+        const undoButton = document.querySelector('.undobutton')
+        const searchBox = document.querySelector('#searchingbox')
+        let draggedNote
 
-    function start(activeNotes) {
-        pastNotes.addEventListener('click', clickOnNote)
-        pastNotes.addEventListener('dragstart', draggingNote)
-        pastNotes.addEventListener('dragend', dragEnds)
-        pastNotes.addEventListener('dragover', function (event) {
-            event.preventDefault()
-        })
-        pastNotes.addEventListener('drop', dropNote)
-        textSpace.addEventListener('keydown', allowTabs)
-        window.addEventListener('keydown', function (event) {
-            if (event.ctrlKey && (event.key === 'z' || event.key === 'Z')){
+        function notifyChangeBox() {
+            const filter = searchBox.value
+            pubsub.publish('newText', filter)
+        };
+
+        function start(activeNotes) {
+            pastNotes.addEventListener('click', clickOnNote)
+            pastNotes.addEventListener('dragstart', draggingNote)
+            pastNotes.addEventListener('dragend', dragEnds)
+            pastNotes.addEventListener('dragover', function (event) {
                 event.preventDefault()
+            })
+            pastNotes.addEventListener('drop', dropNote)
+            textSpace.addEventListener('keydown', allowTabs)
+            window.addEventListener('keydown', function (event) {
+                if (event.ctrlKey && (event.key === 'z' || event.key === 'Z')){
+                    event.preventDefault()
+                }
+            })
+
+            searchBox.addEventListener('keyup', notifyChangeBox)
+            saveButton.addEventListener('click', saveNote)
+            undoButton.addEventListener('click', undoAction)
+            document.addEventListener('keydown', checkKeys)
+            notifyChangeBox(activeNotes)
+        }
+
+        function clickOnNote(ev) {
+            const clicked = ev.target
+            const clickedClass = clicked.getAttribute('class')
+            let dbid = clicked.getAttribute('dbid')
+            switch (clickedClass) {
+                case 'vbutton':
+                    pubsub.publish('viewClicked', dbid)
+                    break
+                case 'ebutton':
+                    pubsub.publish('editClicked', dbid)
+                    break
+                case 'dbutton':
+                    pubsub.publish('deleteNote', dbid)
+                    break
             }
-        })
-
-        searchBox.addEventListener('keyup', notifyChangeBox)
-        saveButton.addEventListener('click', saveNote)
-        undoButton.addEventListener('click', undoAction)
-        document.addEventListener('keydown', checkKeys)
-        notifyChangeBox(activeNotes)
-    }
-
-    function clickOnNote(ev) {
-        const clicked = ev.target
-        const clickedClass = clicked.getAttribute('class')
-        let dbid = clicked.getAttribute('dbid')
-        switch (clickedClass) {
-            case 'vbutton':
-                pubsub.publish('viewClicked', dbid)
-                break
-            case 'ebutton':
-                pubsub.publish('editClicked', dbid)
-                break
-            case 'dbutton':
-                pubsub.publish('deleteNote', dbid)
-                break
         }
-    }
 
-    function editConf(note, dates, dbid) {
-        searchBox.style.display = 'none'
-        activityTitle.textContent = 'Edit this note.'
-        datesBox.style.display = 'inline'
-        textSpace.setAttribute('placeholder', 'Write a new version of the note.')
-        textSpace.readOnly = false
-        saveButton.style.display = 'none'
-        editButton.style.display = 'inline'
-        editButton.textContent = 'Save the changes!'
-        cancelButton.style.display = 'inline'
-        cancelButton.textContent = 'Cancel.'
-        undoButton.style.display = 'none'
-        pastNotes.style.display = 'none'
+        function editConf(note, dates, dbid) {
+            searchBox.style.display = 'none'
+            activityTitle.textContent = 'Edit this note.'
+            datesBox.style.display = 'inline'
+            textSpace.setAttribute('placeholder', 'Write a new version of the note.')
+            textSpace.readOnly = false
+            saveButton.style.display = 'none'
+            editButton.style.display = 'inline'
+            editButton.textContent = 'Save the changes!'
+            cancelButton.style.display = 'inline'
+            cancelButton.textContent = 'Cancel.'
+            undoButton.style.display = 'none'
+            pastNotes.style.display = 'none'
 
-        textSpace.value = note
-        editButton.addEventListener('click', onEditButton, { once: true })
-        const creationDate = dates.creation
-        const lastMDate = dates.modification
-        creationDP.textContent = `Created: ${creationDate}.`
-        lastMDP.textContent = `Last modification: ${lastMDate}`
-        cancelButton.addEventListener('click', onCancelButton, { once: true })
-        function onCancelButton() {
-            editButton.removeEventListener('click', onEditButton, { once: true })
-            editNote(dbid, true).saveEdition()
+            textSpace.value = note
+            editButton.addEventListener('click', onEditButton, { once: true })
+            const creationDate = dates.creation
+            const lastMDate = dates.modification
+            creationDP.textContent = `Created: ${creationDate}.`
+            lastMDP.textContent = `Last modification: ${lastMDate}`
+            cancelButton.addEventListener('click', onCancelButton, { once: true })
+            function onCancelButton() {
+                editButton.removeEventListener('click', onEditButton, { once: true })
+                editNote(dbid, true).saveEdition()
+            }
+            function onEditButton() {
+                cancelButton.removeEventListener('click', onCancelButton, { once: true })
+                editNote(dbid).saveEdition()
+            }
         }
-        function onEditButton() {
-            cancelButton.removeEventListener('click', onCancelButton, { once: true })
-            editNote(dbid).saveEdition()
+
+        function mainConf(activeNotes) {
+            searchBox.style.display = 'inline'
+            activityTitle.textContent = 'Create a note.'
+            datesBox.style.display = 'none'
+            textSpace.setAttribute('placeholder', 'Write a note here.')
+            textSpace.readOnly = false
+            textSpace.value = ''
+            saveButton.style.display = 'inline'
+            saveButton.textContent = 'Save the note!'
+            editButton.style.display = 'none'
+            cancelButton.style.display = 'none'
+            pastNotes.style.display = 'block'
+            undoButton.style.display = 'inline-block'
+            placeNotes(activeNotes)
+            cancelButton.removeEventListener('click', mainConf, { once: true })
         }
-    }
 
-    function mainConf(activeNotes) {
-        searchBox.style.display = 'inline'
-        activityTitle.textContent = 'Create a note.'
-        datesBox.style.display = 'none'
-        textSpace.setAttribute('placeholder', 'Write a note here.')
-        textSpace.readOnly = false
-        textSpace.value = ''
-        saveButton.style.display = 'inline'
-        saveButton.textContent = 'Save the note!'
-        editButton.style.display = 'none'
-        cancelButton.style.display = 'none'
-        pastNotes.style.display = 'block'
-        undoButton.style.display = 'inline-block'
-        placeNotes(activeNotes)
-        cancelButton.removeEventListener('click', mainConf, { once: true })
-    }
+        function editNote(dbid, checkCancel = false) {
+            return {
+                saveEdition: () => {
+                    pubsub.publish('saveEditClicked', [dbid, checkCancel])
+                }
+            }
+        }
 
-    function editNote(dbid, checkCancel = false) {
+        function saveEdition(newNote, dbid, activeNotes, checkCancel) {
+            if (!checkCancel) {
+                newNote = textSpace.value
+            }
+            activeNotes[dbid]['note'] = newNote
+            pubsub.publish('editNote', [newNote, dbid])
+            mainConf(activeNotes)
+        }
+
+        function viewConf(dbid, note, dates) {
+            searchBox.style.display = 'none'
+            activityTitle.textContent = 'Current note.'
+            datesBox.style.display = 'inline'
+            textSpace.readOnly = true
+            saveButton.style.display = 'none'
+            editButton.style.display = 'inline'
+            editButton.textContent = 'Go back!'
+            cancelButton.style.display = 'none'
+            pastNotes.style.display = 'none'
+            textSpace.value = note
+            const creationDate = dates.creation
+            const lastMDate = dates.modification
+            editButton.addEventListener('click', editNote(dbid).saveEdition, { once: true })
+            creationDP.textContent = `Created: ${creationDate}.`
+            lastMDP.textContent = `Last modification: ${lastMDate}`
+            undoButton.style.display = 'none'
+        }
+
+        function currentNote(dbid, activeNotes) {
+            const temp = document.querySelector('#notes')
+            const div = temp.content.querySelector('.pastnote')
+            div.setAttribute('dbid', dbid)
+            const p = div.querySelector('p')
+            p.setAttribute('dbid', dbid)
+            const buttons = div.querySelector('.buttons')
+            buttons.setAttribute('dbid', dbid)
+            const erraseB = buttons.querySelector('.dbutton')
+            erraseB.setAttribute('dbid', dbid)
+            const editB = buttons.querySelector('.ebutton')
+            editB.setAttribute('dbid', dbid)
+            const viewB = buttons.querySelector('.vbutton')
+            viewB.setAttribute('dbid', dbid)
+            const noteData = activeNotes[dbid]
+            p.textContent = noteData.note.slice(0, 10)
+            if (noteData.note.length > 10) {
+                p.textContent += '...'
+            }
+            const a = document.importNode(div, true)
+            return a
+        }
+
+        function viewFactory() {
+            function createNote(dbid, activeNotes) {
+                return currentNote(dbid, activeNotes)
+            }
+            return { createNote: createNote }
+        }
+
+        function placeNotes(activeNotes) {
+            pastNotes.innerHTML = ''
+            const fragment = document.createDocumentFragment()
+            for (const i of Object.keys(activeNotes).reverse()) {
+                const j = parseInt(i)
+                const currentNote = viewFactory().createNote(j, activeNotes)
+                fragment.appendChild(currentNote)
+            }
+            pastNotes.appendChild(fragment)
+        }
+
+        function saveNote() {
+            const note = textSpace.value
+            pubsub.publish('saveNote', note)
+        }
+
+        function allowTabs(event) {
+            // Source: https://stackoverflow.com/questions/6637341/use-tab-to-indent-in-textarea
+            if (event.key === 'Tab') {
+                event.preventDefault()
+                const start = this.selectionStart
+                const end = this.selectionEnd
+                this.value = this.value.slice(0, start) + '\t' + this.value.slice(end)
+                // To place the cursor in the right character
+                this.selectionEnd = start + 1
+            }
+        }
+
+        function draggingNote(event) {
+            draggedNote = event.target
+            draggedNote.style.opacity = 0.3
+        }
+
+        function dragEnds(event) {
+            draggedNote.style.opacity = 1
+        }
+
+        function dropNote(event) {
+            const startingPlace = draggedNote.getAttribute('dbid')
+            const endingPlace = event.target.getAttribute('dbid')
+            pubsub.publish('interchangeNotes', [startingPlace, endingPlace])
+        }
+
+        function checkKeys(event) {
+            if (event.ctrlKey && (event.key === 'z' || event.key === 'Z')) {
+                undoAction()
+            }
+        }
+
+        function undoAction() {
+            if (undoButton.style.display !== 'none') {
+                pubsub.publish('undoAction')
+
+            }
+        }
+
         return {
-            saveEdition: () => {
-                pubsub.publish('saveEditClicked', [dbid, checkCancel])
-            }
+            main: mainConf,
+            edit: editConf,
+            view: viewConf,
+            start: start,
+            saveEdition: saveEdition
         }
     }
-
-    function saveEdition(newNote, dbid, activeNotes, checkCancel) {
-        if (!checkCancel) {
-            newNote = textSpace.value
-        }
-        activeNotes[dbid]['note'] = newNote
-        pubsub.publish('editNote', [newNote, dbid])
-        mainConf(activeNotes)
-    }
-
-    function viewConf(dbid, note, dates) {
-        searchBox.style.display = 'none'
-        activityTitle.textContent = 'Current note.'
-        datesBox.style.display = 'inline'
-        textSpace.readOnly = true
-        saveButton.style.display = 'none'
-        editButton.style.display = 'inline'
-        editButton.textContent = 'Go back!'
-        cancelButton.style.display = 'none'
-        pastNotes.style.display = 'none'
-        textSpace.value = note
-        const creationDate = dates.creation
-        const lastMDate = dates.modification
-        editButton.addEventListener('click', editNote(dbid).saveEdition, { once: true })
-        creationDP.textContent = `Created: ${creationDate}.`
-        lastMDP.textContent = `Last modification: ${lastMDate}`
-        undoButton.style.display = 'none'
-    }
-
-    function currentNote(dbid, activeNotes) {
-        const temp = document.querySelector('#notes')
-        const div = temp.content.querySelector('.pastnote')
-        div.setAttribute('dbid', dbid)
-        const p = div.querySelector('p')
-        p.setAttribute('dbid', dbid)
-        const buttons = div.querySelector('.buttons')
-        buttons.setAttribute('dbid', dbid)
-        const erraseB = buttons.querySelector('.dbutton')
-        erraseB.setAttribute('dbid', dbid)
-        const editB = buttons.querySelector('.ebutton')
-        editB.setAttribute('dbid', dbid)
-        const viewB = buttons.querySelector('.vbutton')
-        viewB.setAttribute('dbid', dbid)
-        const noteData = activeNotes[dbid]
-        p.textContent = noteData.note.slice(0, 10)
-        if (noteData.note.length > 10) {
-            p.textContent += '...'
-        }
-        const a = document.importNode(div, true)
-        return a
-    }
-
-    function viewFactory() {
-        function createNote(dbid, activeNotes) {
-            return currentNote(dbid, activeNotes)
-        }
-        return { createNote: createNote }
-    }
-
-    function placeNotes(activeNotes) {
-        pastNotes.innerHTML = ''
-        const fragment = document.createDocumentFragment()
-        for (const i of Object.keys(activeNotes).reverse()) {
-            const j = parseInt(i)
-            const currentNote = viewFactory().createNote(j, activeNotes)
-            fragment.appendChild(currentNote)
-        }
-        pastNotes.appendChild(fragment)
-    }
-
-    function saveNote() {
-        const note = textSpace.value
-        pubsub.publish('saveNote', note)
-    }
-
-    function allowTabs(event) {
-        // Source: https://stackoverflow.com/questions/6637341/use-tab-to-indent-in-textarea
-        if (event.key === 'Tab') {
-            event.preventDefault()
-            const start = this.selectionStart
-            const end = this.selectionEnd
-            this.value = this.value.slice(0, start) + '\t' + this.value.slice(end)
-            // To place the cursor in the right character
-            this.selectionEnd = start + 1
-        }
-    }
-
-    function draggingNote(event) {
-        draggedNote = event.target
-        draggedNote.style.opacity = 0.3
-    }
-
-    function dragEnds(event) {
-        draggedNote.style.opacity = 1
-    }
-
-    function dropNote(event) {
-        const startingPlace = draggedNote.getAttribute('dbid')
-        const endingPlace = event.target.getAttribute('dbid')
-        pubsub.publish('interchangeNotes', [startingPlace, endingPlace])
-    }
-
-    function checkKeys(event) {
-        if (event.ctrlKey && (event.key === 'z' || event.key === 'Z')) {
-            undoAction()
-        }
-    }
-
-    function undoAction() {
-        if (undoButton.style.display !== 'none') {
-            pubsub.publish('undoAction')
-
-        }
-    }
-
     return {
-        main: mainConf,
-        edit: editConf,
-        view: viewConf,
-        start: start,
-        saveEdition: saveEdition
-    }
-})(pubsub);
+        getInstance: function () {
+          viewInstance = init();
+          return viewInstance;
+        }
+      };
+})(pubsub).getInstance();
 
 
 //* ****************************************************************************************
