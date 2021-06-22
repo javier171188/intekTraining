685a926e53a9dfb31debef2b63523b0347102d54
javier171188 <javier171188@hotmail.com>
2021-06-22 12:01:31 -0500

    exercise 12 done

--- es6/e12.js
+++ es6/e12.js
@@ -1,1 +1,16 @@
 'use strict';
+let resultP = document.querySelector('.result');
+
+function modifyDateObject(name,fun){
+    Date.prototype[name] = fun;
+}
+
+function customDate(){
+    return 'January 12';
+}
+
+modifyDateObject('constantDate', customDate);
+
+let date = new Date();
+
+resultP.textContent = date.constantDate();
