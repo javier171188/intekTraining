aa77c686e64393330239fe00cd391b623f0e5bf9
javier171188 <javier171188@hotmail.com>
2021-06-24 16:34:25 -0500

    exercise 23 done

--- es6/e23.js
+++ es6/e23.js
@@ -1,29 +1,67 @@
 'use strict';
 let min = -27;
 let max = 10;
+let genButton = document.getElementById('genera');
+let minBox = document.getElementById('min');
+let maxBox = document.getElementById('max');
+let nextButton = document.getElementById('next');
+let textArea = document.querySelector('.text-area');
 
-let rand = getRandom(-5,5);
-for (let i = 0; i<30; i++){
-    console.log(rand());
-    //rand();
-}
 
+genButton.addEventListener('click', startShowing);
 
-function getRandom(min,max){
-    let allNumbers = [];
-    for (let n = min; n<=max; n++){
-        allNumbers.push(n);
-    }
-    
-    function getNumber(){
-        let range = allNumbers.length;
-        if (range <= 0){
-            return `There are no more numbers in the interval`// [${min}, ${max}]`;
+function startShowing(){
+    let minNum = minBox.value;
+    let maxNum = maxBox.value;
+    genButton.style.display = 'none';
+    nextButton.style.display = 'inline-block';
+    nextButton.value = 'Next'
+    maxBox.disabled = true;
+    minBox.disabled = true;
+
+    let rand = getRandom(minNum,maxNum);
+    nextButton.addEventListener('click', applyNext);
+
+    function applyNext(){
+        if(textArea.textContent.includes('There')){
+            nextButton.removeEventListener('click', applyNext);
+            genButton.style.display = 'inline-block';
+            nextButton.style.display = 'none';
+            maxBox.disabled = false;
+            minBox.disabled = false;
+            textArea.textContent = '';
+        }else{
+            textArea.textContent = rand();
         }
-        let index = Math.floor(Math.random()*range);
-        let randomResult = allNumbers[index];
-        allNumbers.splice(index, 1);
-        return randomResult;
+        if(textArea.textContent.includes('There')){
+            nextButton.value = 'Try again';
+        }
     }
-    return getNumber;
+
+
+    function getRandom(min,max){
+        let allNumbers = [];
+        for (let n = min; n<=max; n++){
+            allNumbers.push(n);
+        }
+        
+        function getNumber(){
+            let range = allNumbers.length;
+            if (range <= 0){
+                return `There are no more numbers in the interval [${min}, ${max}]`;
+            }
+            let index = Math.floor(Math.random()*range);
+            let randomResult = allNumbers[index];
+            allNumbers.splice(index, 1);
+            return randomResult;
+        }
+        return getNumber;
+    }
 }
+
+
+
+
+
+
+
