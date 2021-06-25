d375dbc00f361f2688f0367f8fe3518f80a2f6cc
javier171188 <javier171188@hotmail.com>
2021-06-25 16:53:08 -0500

    exercise 30 done

---
+++ es6/e30.js
@@ -0,0 +1,18 @@
+'use strict';
+let paragraphs = document.getElementsByTagName('p');
+
+
+console.log(paragraphs);
+
+for (let i=0; i< paragraphs.length; i = i+2){
+    paragraphs[i].addEventListener('click', ()=>{changeState(i); });
+}
+
+function changeState(i){
+    if (paragraphs[i+1].style.visibility === 'hidden' || paragraphs[i+1].style.visibility === ''){
+        paragraphs[i+1].style.visibility = 'visible';
+    }
+    else{
+        paragraphs[i+1].style.visibility = 'hidden';
+    }
+}
