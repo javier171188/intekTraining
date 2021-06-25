34253d301c52c9a0b74d2f8813e4ed786e32636a
javier171188 <javier171188@hotmail.com>
2021-06-25 14:35:34 -0500

    works for array of dim 2

--- es6/e28.js
+++ es6/e28.js
@@ -1,1 +1,28 @@
 'use strict';
+let url1 = 'https://jsonplaceholder.typicode.com/posts',
+    url2 = 'https://jsonplaceholder.typicode.com/users';
+
+
+var async = {
+    getAll: function ( urlArray, callback){ 
+            let obj;
+            //let f =  fetch(urlArray[0]).then(data => data.json())
+            //let g = fetch(urlArray[0]).then(data => data.json())
+            //Promise.all([f(),g()]).then(response => console.log(response));
+            const first = () => {
+                return fetch(urlArray[0]).then(data => data.json());
+                }
+            const second = () => {
+                return fetch(urlArray[0]).then(data => data.json());
+            }
+
+            Promise.all([first(),second()]).then(response => console.log(response));
+
+    } };
+
+//async.getAll([axCall1,axCall2], callback);
+async.getAll([url1,url2], finalFunction);
+
+function finalFunction(x){
+    console.log(x);
+}
