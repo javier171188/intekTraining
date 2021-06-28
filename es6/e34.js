4aec7bcccf2f3948df0e36c96b47e5a83125d75e
javier171188 <javier171188@hotmail.com>
2021-06-28 12:35:41 -0500

    exercise 34 done

--- es6/e34.js
+++ es6/e34.js
@@ -1,26 +1,41 @@
 'use strict';
+let button = document.querySelector('.start-button');
+let heightT = document.querySelector('.height');
+
 let g = -0.00259,
-    v0 = 0,
-    s0 = 9,
-    t = 0,
-    final = 8,
     y;
 
+button.addEventListener('click', startCount);
 
-for (let i =0; i< 1000; i++){
-    t++;
-    y = position(t,v0,s0);
-    console.log(y);
-    if (y<=0){
-        t = 0;
-        v0 =  Math.sqrt(- 2*final*g);
-        s0 = 0;
-        final--
+function startCount(){
+    let   v0 = 0,
+    s0 = 10,
+    t = 0,
+    final = 9;
+ 
+    let inter = setInterval(fall, 50);
+    button.style.display = 'none';
+    function fall(){
+        y = position(t,v0,s0);
+        if (y<=0){
+            t = 0;
+            v0 =  Math.sqrt(- 2*final*g);
+            s0 = 0;
+            final--;
+            y=0;
+            if (final < 0){
+                clearInterval(inter);
+                button.style.display = 'block';
+            }  
+        }
+        heightT.textContent = `${String(y).substring(0,4)} in.`
+        t++;
+    }
+    
+    function position(t,v0,s0){
+        return g*t*t/2 + v0*t + s0;
     }
 }
 
 
 
-function position(t,v0,s0){
-    return g*t*t/2 + v0*t + s0;
-}
