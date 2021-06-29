74bfcce5bcd09247de151faceefc76ff1ba7b7f1
javier171188 <javier171188@hotmail.com>
2021-06-28 19:45:36 -0500

    working on exercise 38

---
+++ es6/e38.js
@@ -0,0 +1,65 @@
+'use strict';
+
+class Calculator{
+    constructor(initalValue = 0){
+        this.value = initalValue;
+    }
+
+    add(number){
+        this.value = this.value + number;
+    }
+
+    substract(number){
+        this.value = this.value - number;
+    }
+
+    multiply(number){
+        this.value = this.value*number;
+    }
+
+    divide(number){
+        if (number !== 0){
+            this.value = this.value/number;
+        }
+    }
+}
+
+let calculator = new Calculator;
+let resultArea = document.getElementById('result');
+let zeroDiv = document.querySelector('.zerodiv');
+
+resultArea.textContent = calculator.value;
+
+let numberArea = document.getElementById('number-area');
+let buttons = document.querySelector('.buttons');
+
+buttons.addEventListener('click', operate)
+
+function operate(event){
+    let clicked = event.target;
+    let action = clicked.id;
+    let value = numberArea.value;
+    value = parseFloat(value);
+    zeroDiv.textContent = '';
+    if (value === 0 && action === 'divide'){
+        zeroDiv.textContent = "We can't divide by zero."
+        
+    }
+    console.log(calculator.value);
+    switch (action){
+        case 'add':
+            calculator.add(value);
+        break;
+        case 'add':
+            calculator.add(value);
+        break;
+        case 'add':
+            calculator.add(value);
+        break;
+        case 'add':
+            calculator.add(value);
+        break;
+    }
+    console.log(calculator.value);
+    resultArea.textContent = calculator.value;
+}
