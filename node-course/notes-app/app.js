4824fcf84d0d1b52017d9b3ad91215544a2e4951
javier171188 <javier171188@hotmail.com>
2021-06-30 12:49:09 -0500

    chapter 17 done

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -2,5 +2,58 @@ const getNotes = require('./notes.js')
 const yargs = require('yargs')
 const chalck = require('chalk')
 
-console.log(process.argv)
-console.log(yargs.argv)
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    'command': 'add',
+    'describe': 'Add a new note',
+    'builder': {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Body of the note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log(`Title: ${argv.title}`)
+        console.log(`Body: ${argv.body}`)
+    }
+})
+
+// Create remove command
+yargs.command({
+    'command': 'remove',
+    'describe': 'Remove  note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    'command': 'list',
+    'describe': 'List the notes',
+    handler: function(){
+        console.log('Listing out all notes')
+    }
+})
+
+// Create read command
+yargs.command({
+    'command': 'read',
+    'describe': 'Reading the note',
+    handler: function(){
+        console.log('Reading the note')
+    }
+})
+
+// add, remove, read, list
+
+yargs.parse()
