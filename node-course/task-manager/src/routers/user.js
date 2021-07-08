2a7be9cf4344f40c8cce8b13cd616ee61ad399c1
javier171188 <javier171188@hotmail.com>
2021-07-07 19:36:16 -0500

    Before starting lesson 131

--- node-course/task-manager/src/routers/user.js
+++ node-course/task-manager/src/routers/user.js
@@ -2,6 +2,8 @@ const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
+const multer = require('multer')
+const sharp = require('sharp')
 
 router.get('/test', (req, res) => {
     res.send('From a new file')
@@ -30,33 +32,36 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', auth, async (req,res) => {
-    try {
-        const users = await User.find({})
-        res.status(200).send(users)
-    } catch (e){
+router.post('/users/logout', auth, async (req, res) => {
+    try{
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch(e){
         res.status(500).send()
     }
-    
 })
 
-router.get('/users/:id', async (req, res) =>{
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        if(!user){
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (e){
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try{
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch(e){
         res.status(500).send()
     }
+})
     
-    
+
+router.get('/users/me', auth, async (req,res) => {
+    res.send(req.user)    
 })
 
-router.patch('/users/:id', async (req, res) => {
+
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -67,34 +72,66 @@ router.patch('/users/:id', async (req, res) => {
 
     try {
 
-        const user = await User.findById(req.params.id)
+        updates.forEach((update) => req.user[update] = req.body[update])
 
-        updates.forEach((update) => user[update] = req.body[update])
+        await req.user.save()
 
-        await user.save()
-
-        if (!user){
-            return res.status(404).send()
-        }
-        res.send(user)
+        res.send(req.user)
     } catch (e){
         res.status(400).send(e)
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id) 
-
-        if (!user){
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
 
     } catch (e){
         res.status(500).send()
     }
 })
 
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            return cb(new Error('Please upload an image'))
+        }
+
+        cb(undefined, true)
+    }
+})
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req,res) => {
+    const buffer = await sharp(req.file.buffer).resize({width:250, height:250}).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error:error.message})
+})
+
+router.delete('/users/me/avatar', auth, async (req,res) =>{
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error:error.message})
+})
+
+router.get('/users/:id/avatar', async (req,res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if(!user || !user.avatar){
+            throw new Error()
+        }
+        res.set('Content-Type','image/jpg')
+        res.send(user.avatar)
+    } catch (e){
+        res.status(404).send()
+    }
+})
 module.exports = router
