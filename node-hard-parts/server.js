64bf095a2d3af2afab559cc06b4c451e7f250ece
javier171188 <javier171188@hotmail.com>
2021-07-15 19:46:15 -0500

    Working on the exercises of the hard parts node

--- node-hard-parts/server.js
+++ node-hard-parts/server.js
@@ -1,28 +1,74 @@
 const http = require('http');
 const fs = require('fs');
+const pug = require('pug');
 
 function doOnRequest(request, response){
   // Send back a message saying "Welcome to Twitter"
   // code here...
-  response.end("yo")  
+  //response.write("Welcome to Twitter")  
   if (request.method === 'GET' && request.url === '/') {
     // read the index.html file and send it back to the client
     // code here...
-    fs.createReadStream()
+    let html = fs.readFileSync('index.html');
+    response.writeHeader(200, {"Content-Type": "text/html"});  
+    response.write(html);  
+    response.end()
+    //fs.createReadStream()
 
   }
   else if (request.method === 'POST' && request.url === '/sayHi') {
     // code here...
+    response.write('hi back to you!');  
+    response.end();  
+    fs.appendFileSync('hi_log.txt',  'Somebody said hi.\n')
     
   }
   else if (request.method === 'POST' && request.url === '/greeting') {
     // accumulate the request body in a series of chunks
     // code here...
+    let body = '';
+    request.on('data', chunk => {
+      body += chunk.toString();
+    });
+    request.on('end', () => {
+      if (body === 'hello'){
+        response.end('hello there!');
+      } else if (body === "what's up"){
+        response.end("the sky");
+      }
+      else{
+        response.end("good morning")
+      }
+      fs.appendFileSync('hi_log.txt',  body + '\n');
+  });
     
+  }
+  else if (request.method === 'GET' && request.url === '/style.css'){
+    let css = fs.readFileSync('style.css');
+    response.writeHead(200, {"Content-Type": "text/css"});
+    response.write(css);
+    response.end();
+  }
+  else if (request.method === 'PUT' && request.url === '/change'){
+    let body = '';
+    request.on('data', chunk => {
+      body += chunk.toString();
+    });
+    
+    request.on('end', () => {
+      fs.writeFileSync('hi_log.txt', body + '\n')
+      response.end();
+    })
+
+  } else if (request.method === 'DELETE' && request.url === '/delete') {
+      fs.unlinkSync('hi_log.txt');
+      response.end();
   }
   else {
     // Handle 404 error: page not found
-    // code here...
+    response.setStatusCode = 404;
+    response.end('Error: Not Found')
+    
     
   }
 }
--- node-hard-parts/streams/streams.js
+++ node-hard-parts/streams/streams.js
@@ -1,9 +1,22 @@
 const fs = require('fs');
 const through = require('through2');
+let data = '';
 
-/* Create a read stream here
-const readPoemStream = ???
-*/
+/* Create a read stream here*/
+const readPoemStream = fs.createReadStream('streams/on-joy-and-sorrow-emoji.txt'); 
+
+readPoemStream.setEncoding('UTF8'); 
+
+readPoemStream.on('data', function(chunk) {
+    //data += chunk.toString().replace(':)', 'joy').replace(':(', 'sorrow')
+    data += chunk.substring(4);
+    
+    
+ });
+ 
+ readPoemStream.on('end',function() {
+    console.log( data);
+ });
 
 /* Create a write stream here
 const writePoemStream =
