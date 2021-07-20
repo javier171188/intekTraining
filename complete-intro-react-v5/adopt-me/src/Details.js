81b86beedd6967b1fb1ae00fb86c41f75593f6dd
javier171188 <javier171188@hotmail.com>
2021-07-20 16:58:44 -0500

    v5 finished

--- complete-intro-react-v5/adopt-me/src/Details.js
+++ complete-intro-react-v5/adopt-me/src/Details.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import pet from '@frontendmasters/pet';
+import { navigate } from '@reach/router';
+import Modal from './Modal';
 import Carousel from './Carrousel';
 import ErrorBoundary from './ErrorBoundary';
 import ThemeContext from './ThemeContext';
@@ -9,7 +11,8 @@ class Details extends React.Component {
         super(props);
 
         this.state = {
-            loading: true
+            loading: true,
+            showModal: false
         }
     }
     
@@ -17,6 +20,7 @@ class Details extends React.Component {
         pet.animal(this.props.id)
             .then(({ animal }) => {
                 this.setState({
+                    url: animal.url,
                     name: animal.name,
                     animal: animal.type,
                     location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
@@ -27,13 +31,14 @@ class Details extends React.Component {
                 })
             }, console.error)
     }
-    
+    toggleModal = () => this.setState({ showModal: !this.state.showModal });
+    adopt = () => navigate(this.state.url);
     render(){
         if (this.state.loading){
             return <h1>loading ...</h1>
         }
         
-        const { animal, breed, location, description, name, media} = this.state;
+        const { animal, breed, location, description, name, media, showModal} = this.state;
 
         return (
             <div className="details">
@@ -43,7 +48,7 @@ class Details extends React.Component {
                     <h2>{`${animal} - ${breed} -${location}`}</h2>
                     <ThemeContext.Consumer>
                         {([theme]) => (
-                            <button style={{ backgroundColor: theme}}>
+                            <button onClick={this.toggleModal} style={{ backgroundColor: theme}}>
                                 Adopt {name}
                             </button>
                         )}
@@ -52,6 +57,19 @@ class Details extends React.Component {
 
                     
                     <p>{description}</p>
+                    {
+                        showModal ? (
+                            <Modal>
+                                <div>
+                                    <h1>Would you like to adopt {name}?</h1>
+                                    <div className="buttons">
+                                        <button onClick={this.adopt}>Yes</button>
+                                        <button onClick={this.toggleModal}>No, I am a monster</button>
+                                    </div>
+                                </div>
+                            </Modal>
+                        ) : null 
+                    }
                 </div>
             </div>
         )
