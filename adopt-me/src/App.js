59e33184c7d3563b6c6631292ef93aedea41cf52
javier171188 <javier171188@hotmail.com>
2021-07-20 19:46:01 -0500

    Working on Eslint & React

---
+++ adopt-me/src/App.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
+
+
+const App = () => {
+    return (
+    <div>
+        <h1>Adopt Me!</h1>
+        <Pet name="Luna" animal="Dog" breed="Havanese" />
+        <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+        <Pet name="Beam" animal="Dog" breed="Wheaten Terrier" />
+    </div>
+    );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
