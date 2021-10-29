ed523bef374e4f1b47145fa83bf93abcb0e1c52d
javier171188 <javier171188@hotmail.com>
2021-10-28 19:32:02 -0500

    Exercice 2 seems to be working

--- FullCourseReview/promises/exercise2/exercise2.js
+++ FullCourseReview/promises/exercise2/exercise2.js
@@ -1,18 +1,4 @@
 'use strict'
-import fetch from "node-fetch";
-const urlQuery = url => () => fetch(url)
-
-const maxRetry = 3;
-const useIncrement = true;
-const delay = 1000;
-
-/**
-* perform query successfully once or try up to a maximum of maxRetry times
-* if unsuccessful, delay the next attempt for an amount of time. If attempts
-* continue to fail then increase the delay between attempts if useIncrements
-* is set to true. 
-*/
-
 
 function wait(milliseconds) {
     var start = new Date().getTime();
@@ -24,29 +10,24 @@ function wait(milliseconds) {
     }
 }
 
-
 async function queryRetry(request, maxRetry, delay, useIncrement) {
     let result;
     let waitTime = delay;
+    let start = new Date().getTime();
     for (let i = 0; i < maxRetry; i++) {
         try {
             result = await request();
             return result
         } catch (e) {
-
-        }
-        wait(waitTime);
-        if (useIncrement) {
-            waitTime += delay
+            if (i !== maxRetry - 1) {
+                wait(waitTime);
+            }
+            if (useIncrement) {
+                waitTime += delay
+            }
         }
     }
-    throw new Error(`Query rejected ${maxRetry} times.`)
+    throw { Error: `Query rejected ${maxRetry} times.` }
 }
 
-queryRetry(urlQuery('https://www.google.com/'), maxRetry, delay, useIncrement)
-    .then((r) => console.log('success'))
-    .catch(() => console.log('error'));
-
-queryRetry(urlQuery('https://www.gle.com/'), maxRetry, delay, useIncrement)
-    .then((r) => console.log('success'))
-    .catch(console.log);
+module.exports = queryRetry;
---
+++ FullCourseReview/promises/exercise2/exercise2.unit.test.js
@@ -0,0 +1,69 @@
+'use strict'
+const axios = require('axios');
+const queryRetry = require('./exercise2');
+
+const urlQuery = url => () => axios.get(url)
+
+const maxRetry = 3;
+const useIncrement = true;
+const delay = 1000;
+
+
+
+test('Success in the first try.', () => {
+    expect.assertions(1);
+    return expect(queryRetry(urlQuery('https://www.google.com/'), maxRetry, delay, useIncrement))
+        .resolves
+        .toBeTruthy();
+})
+
+const successOnSecondCall = () => {
+    let numberOfCall = 0;
+    return () => {
+        numberOfCall += 1;
+        return new Promise((res, rej) => setTimeout(numberOfCall > 1 ? res : rej, 1000, 'success'))
+    }
+};
+
+test('Success in the second try.', () => {
+    var query = successOnSecondCall();
+
+    return expect(queryRetry(query, maxRetry, delay, useIncrement))
+        .resolves
+        .toEqual('success');
+})
+
+
+test('Fail.', () => {
+    return expect(queryRetry(urlQuery('https://www.gle.com/'), maxRetry, delay, useIncrement))
+        .rejects.toEqual({
+            Error: 'Query rejected 3 times.',
+        });
+})
+
+test('Check with increment.', async () => {
+    let start = new Date().getTime();
+    let totalTime
+    try {
+        await queryRetry(urlQuery('https://www.gle.com/'), maxRetry, delay, useIncrement);
+    } catch (e) {
+        totalTime = new Date().getTime() - start;
+    }
+    expect(totalTime).toBeGreaterThanOrEqual(3000);
+    expect(totalTime).toBeLessThan(3500);
+    return
+})
+
+test('Check without increment.', async () => {
+    let start = new Date().getTime();
+    let totalTime
+    try {
+        await queryRetry(urlQuery('https://www.gle.com/'), maxRetry, delay, false);
+    } catch (e) {
+        totalTime = new Date().getTime() - start;
+    }
+    expect(totalTime).toBeGreaterThanOrEqual(2000);
+    expect(totalTime).toBeLessThan(2500);
+    return
+})
+
