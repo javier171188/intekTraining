3ce411fe6837ebc4fc6da7521ca3a8a0ee963eee
javier171188 <javier171188@hotmail.com>
2021-11-01 13:21:07 -0600

    Exercise 7 is done without tests

---
+++ FullCourseReview/algorithms/exercise7/exercise7.js
@@ -0,0 +1,27 @@
+'use strict'
+const input = "111";
+
+longestRunOfTwoNumbers(input);
+
+function longestRunOfTwoNumbers(numberString) {
+    if (numberString.length < 3) {
+        return numberString;
+    }
+    let strLength = numberString.length;
+    let runStart = 0;
+    let longestRun = numberString[0];
+
+    for (let i = 1; i < strLength; i++) {
+        let substring = numberString.slice(runStart, i + 1)
+        let numbers = new Set(substring);
+        if (numbers.size < 3 && substring.length > longestRun.length) {
+            longestRun = substring;
+        } else if (numbers.size > 2) {
+            runStart++;
+        }
+
+    }
+    return longestRun;
+}
+
+module.exports = longestRunOfTwoNumbers;
