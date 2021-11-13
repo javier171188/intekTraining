b42709f4b592bc1c445a845ea44f8773a399eef3
javier171188 <javier171188@hotmail.com>
2021-11-12 19:20:23 -0600

    prefix seems to be working

--- FullCourseReview/coreJavaScript/exercise10/exercise10.js
+++ FullCourseReview/coreJavaScript/exercise10/exercise10.js
@@ -2,29 +2,30 @@
 
 const bTree = '(A,(B,(D),(E)),(C,(F,(H),(I)),(G,,(J))))';
 
-`(A,
-    (B,
-        (D),
-        (E)
-    ),
-    (C,
-        (F,(H),(I)), 
-        (G,,(J)))
- )`
-
-printTree(bTree);
-
+printTree('(A,(B,(D),(E)),(C,(F,(H),(I)),(G,,(J))))', 'prefix');
+//printTree('(B,(D),(E))', 'prefix');
 
 function printTree(tree, order = 'infix') {
     tree = tree.replace(' ', '');
     tree = tree.slice(1, -1);
+
     let firstComaIndex = tree.indexOf(',');
+    if (firstComaIndex < 0) {
+        console.log(tree);
+        return;
+    }
+
+
     let root = tree.slice(0, firstComaIndex);
     let branches = tree.slice(firstComaIndex + 1);
 
+    if (branches.length < 1) {
+        console.log(root);
+        return;
+    }
+
     let openParents = 0;
     let closingParIndex;
-
 
     let i;
     for (i in branches) {
@@ -38,9 +39,32 @@ function printTree(tree, order = 'infix') {
             break;
         }
     }
-    let branch1 = branches.slice(0, closingParIndex + 1);
-    let branch2 = branches.slice(closingParIndex + 1,);
 
-    console.log(branch1, branch2);
+
+
+    closingParIndex = parseInt(closingParIndex);
+    let branch1;
+    let branch2;
+    if (closingParIndex === 0) {
+        [branch1, branch2] = branches.split(',')
+    } else {
+        branch1 = branches.substring(0, closingParIndex + 1);
+        branch2 = branches.slice(closingParIndex + 2);
+    }
+
+    /*console.log('root:', root);
+    console.log('branches:', branches)
+    console.log('branch 1', branch1);
+    console.log('branch 2', branch2);
+    console.log('#######################');*/
+    if (order === 'prefix') {
+        console.log(root);
+        printTree(branch1, order);
+        printTree(branch2, order);
+    } else if (order === 'postfix') {
+
+    } else {
+
+    }
 }
 
