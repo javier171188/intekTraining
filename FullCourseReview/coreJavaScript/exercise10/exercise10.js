8c6ee9bc2fbcc0b972eeb7dd66e2203784e528df
javier171188 <javier171188@hotmail.com>
2021-11-16 12:02:02 -0600

    Exercise 10 seems to be done

--- FullCourseReview/coreJavaScript/exercise10/exercise10.js
+++ FullCourseReview/coreJavaScript/exercise10/exercise10.js
@@ -1,71 +1,68 @@
-'use strict'
-
-const bTree = '(A,(B,(D),(E)),(C,(F,(H),(I)),(G,,(J))))';
-
-printTree('(A,(B,(D),(E)),(C,(F,(H),(I)),(G,,(J))))');
-//printTree('(B,(D),(E))', 'prefix');
+'use strict';
 
 function printTree(tree, order = 'infix') {
-    tree = tree.replace(' ', '');
-    tree = tree.slice(1, -1);
+    let transverse = '';
 
-    let firstComaIndex = tree.indexOf(',');
-    if (firstComaIndex < 0) {
-        console.log(tree);
-        return;
-    }
+    function transverseTree(tree, order) {
+        tree = tree.replace(' ', '');
+        tree = tree.slice(1, -1);
 
-    let root = tree.slice(0, firstComaIndex);
-    let branches = tree.slice(firstComaIndex + 1);
-
-    if (branches.length < 1) {
-        console.log(root);
-        return;
-    }
-
-    let openParents = 0;
-    let closingParIndex;
-
-    let i;
-    for (i in branches) {
-        if (branches[i] === ')') {
-            openParents -= 1;
-        } else if (branches[i] === '(') {
-            openParents += 1;
+        let firstComaIndex = tree.indexOf(',');
+        if (firstComaIndex < 0) {
+            transverse += ',' + tree;
+            return;
         }
-        if (openParents === 0) {
-            closingParIndex = i;
-            break;
+
+        let root = tree.slice(0, firstComaIndex);
+        let branches = tree.slice(firstComaIndex + 1);
+
+        if (branches.length < 1) {
+            transverse += ',' + root;
+            return;
+        }
+
+        let openParents = 0;
+        let closingParIndex;
+
+        let i;
+        for (i in branches) {
+            if (branches[i] === ')') {
+                openParents -= 1;
+            } else if (branches[i] === '(') {
+                openParents += 1;
+            }
+            if (openParents === 0) {
+                closingParIndex = i;
+                break;
+            }
+        }
+
+        closingParIndex = parseInt(closingParIndex);
+        let branch1;
+        let branch2;
+        if (closingParIndex === 0) {
+            [branch1, branch2] = branches.split(',')
+        } else {
+            branch1 = branches.substring(0, closingParIndex + 1);
+            branch2 = branches.slice(closingParIndex + 2);
+        }
+
+        if (order === 'prefix') {
+            transverse += ',' + root;
+            transverseTree(branch1, order);
+            transverseTree(branch2, order);
+        } else if (order === 'postfix') {
+            transverseTree(branch1, order);
+            transverseTree(branch2, order);
+            transverse += ',' + root;
+        } else {
+            transverseTree(branch1, order);
+            transverse += ',' + root;
+            transverseTree(branch2, order);
         }
     }
-
-    closingParIndex = parseInt(closingParIndex);
-    let branch1;
-    let branch2;
-    if (closingParIndex === 0) {
-        [branch1, branch2] = branches.split(',')
-    } else {
-        branch1 = branches.substring(0, closingParIndex + 1);
-        branch2 = branches.slice(closingParIndex + 2);
-    }
-
-    /*console.log('root:', root);
-    console.log('branches:', branches)
-    console.log('branch 1', branch1);
-    console.log('branch 2', branch2);
-    console.log('#######################');*/
-    if (order === 'prefix') {
-        console.log(root);
-        printTree(branch1, order);
-        printTree(branch2, order);
-    } else if (order === 'postfix') {
-        printTree(branch1, order);
-        printTree(branch2, order);
-        console.log(root);
-    } else {
-        printTree(branch1, order);
-        console.log(root);
-        printTree(branch2, order);
-    }
+    transverseTree(tree, order);
+    return transverse.slice(1);
 }
 
+module.exports = { printTree };
--- FullCourseReview/coreJavaScript/exercise10/exercise10.unit.test.js
+++ FullCourseReview/coreJavaScript/exercise10/exercise10.unit.test.js
@@ -1,1 +1,21 @@
-'use strict'
+'use strict';
+const { printTree } = require('./exercise10');
+
+test('Basic tests', () => {
+    let tree = '(A,(B,(D),(E)),(C,(F,(H),(I)),(G,,(J))))';
+
+    let prefix = printTree(tree, 'prefix');
+    expect(prefix).toEqual('A,B,D,E,C,F,H,I,G,,J');
+
+    let infix = printTree(tree, 'infix');
+    expect(infix).toEqual('D,B,E,A,H,F,I,C,,G,J');
+
+    let postfix = printTree(tree, 'postfix');
+    expect(postfix).toEqual('D,E,B,H,I,F,,J,G,C,A');
+})
+
+test('Default to infix', () => {
+    let tree = '(A,(B,(D),(E)),(C,(F,(H),(I)),(G,,(J))))';
+    let infix = printTree(tree);
+    expect(infix).toEqual('D,B,E,A,H,F,I,C,,G,J');
+})
