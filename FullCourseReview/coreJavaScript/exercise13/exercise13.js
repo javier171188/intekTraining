259caed2387b2dfabcc2c6c6f499cde1af54c780
javier171188 <javier171188@hotmail.com>
2021-11-16 19:46:03 -0600

    Working on problem 13

---
+++ FullCourseReview/coreJavaScript/exercise13/exercise13.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let tree = `(0, 
+    (1), 
+    (2, 
+        (1),
+        (5, 
+            (3),
+            (5,
+                (6)), 
+            (9))),
+    (3,  
+        (0)),
+    (5), 
+    (7, 
+        (3, 
+            (3), 
+            (0, 
+                (9), 
+                (4)))))`;
+
+function isSameLevel(tree, number1, number2) {
+    let deeps = {};
+    if (number1 === number2) {
+
+    }
+}
