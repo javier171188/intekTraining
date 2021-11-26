0cdfc08053dc165b00d174342583e9b97f45a710
javier171188 <javier171188@hotmail.com>
2021-11-25 19:47:00 -0600

    Working on the tests of exercise 18

--- FullCourseReview/coreJavaScript/exercise18/exercise18.js
+++ FullCourseReview/coreJavaScript/exercise18/exercise18.js
@@ -1,17 +1,6 @@
 'use strict';
 
-let obj = {
-    path: {
-        to: {},
-        otherKey: ''
-    },
-    moreKeys: {}
-}
-
-set(obj, 'path.to.deeply.nested.property', 42);
-console.log(obj.path.to);
 function set(obj, path, value) {
-
     let keys = path.split('.');
     let temp = obj;
     for (let i = 0; i < keys.length; i++) {
@@ -37,3 +26,5 @@ function set(obj, path, value) {
     }
     return obj;
 }
+
+module.exports = { set };
--- FullCourseReview/coreJavaScript/exercise18/exercise18.unit.test.js
+++ FullCourseReview/coreJavaScript/exercise18/exercise18.unit.test.js
@@ -1,2 +1,13 @@
 'use strict';
+const { set } = require('./exercise18');
 
+let obj = {
+    path: {
+        to: {},
+        otherKey: ''
+    },
+    moreKeys: {}
+}
+
+set(obj, 'path.to.deeply.nested.property', 42);
+console.log(obj.path.to);
