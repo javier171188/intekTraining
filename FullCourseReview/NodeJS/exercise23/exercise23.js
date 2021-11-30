e794b0986f33fab629ba583c6cb2ba8a0fe35ee3
javier171188 <javier171188@hotmail.com>
2021-11-30 14:13:02 -0600

    Loading bar seems to be working

--- FullCourseReview/NodeJS/exercise23/exercise23.js
+++ FullCourseReview/NodeJS/exercise23/exercise23.js
@@ -1,14 +1,16 @@
 'use strict';
-const readline = require('readline').createInterface({
+import chalk from 'chalk';
+
+
+/*const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
-let N;
-readline.question('How many primes do you want? ', number => {
+let N;*/
+/*readline.question('How many prime numbers do you want? ', (number) => {
     N = number;
     let NInt = parseInt(N);
     let primeNumbers = [];
-
 
     let propose = 2;
     while (primeNumbers.length < NInt) {
@@ -21,7 +23,41 @@ readline.question('How many primes do you want? ', number => {
     console.log(primeNumbers);
     readline.close();
 });
+*/
+let primeNumbers = [];
+let NInt = 4;
+let propose = 2;
 
+while (primeNumbers.length < NInt) {
+    let i = primeNumbers.length;
+    if (isPrime(propose)) {
+        primeNumbers.push(propose);
+    }
+    propose++;
+}
+
+console.log(primeNumbers);
+
+
+
+logBar(1);
+
+/*function main() {
+    for (let i = 0; i <= 40; i++) {
+        const dots = "█".repeat(i)
+        const left = 40 - i
+        const empty = "░".repeat(left)
+        process.stdout.write(chalk.blue.bgBlack.bold(`\r[${dots}${empty}] `) + chalk.greenBright.bgBlack(`${Math.round(i * 2.5)}% `));
+
+    }
+    return;
+}
+
+main();*/
+
+function wait(ms) {
+    return new Promise(res => setTimeout(res, ms))
+}
 
 function isPrime(number) {
     if (number === 0 || number === 1) return false
@@ -29,4 +65,12 @@ function isPrime(number) {
         if (number % i === 0) return false;
     }
     return true;
+}
+
+function logBar(prop) {
+    let size = 40;
+    let propTotal = parseInt(prop * size);
+    const dots = "█".repeat(propTotal);
+    const empty = "░".repeat(size - propTotal);
+    process.stdout.write(chalk.blue.bgBlack.bold(`\r[${dots}${empty}] `) + chalk.greenBright.bgBlack(`${Math.round(prop * 100)}% `));
 }
