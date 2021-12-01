7477197c3dcf4aa70ded99a771128e963a49c53e
javier171188 <javier171188@hotmail.com>
2021-12-01 16:48:17 -0600

    Unnecesary files deleted

--- FullCourseReview/NodeJS/getUserInput/__mocks__/readlineS.js
+++
@@ -1,9 +0,0 @@
-const myMock = jest.fn();
-
-myMock.mockReturnValueOnce(10)
-module.exports = {
-    createInterface: jest.fn().mockReturnValue({
-        question: jest.fn().mockImplementationOnce((_questionTest, cb) => cb(myMock())),
-        close: jest.fn().mockImplementationOnce(() => undefined)
-    })
-}
