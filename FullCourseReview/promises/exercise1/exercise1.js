e6ad142dfeb49ddbb6b0957ed239486f25c23f2a
javier171188 <javier171188@hotmail.com>
2021-12-15 16:05:08 -0600

    Exercise 1 seems to be corrected without tests

--- FullCourseReview/promises/exercise1/exercise1.js
+++ FullCourseReview/promises/exercise1/exercise1.js
@@ -1,5 +1,68 @@
-'use strict'
+'use strict';
 
+const taskFactorySample = (delay, resolve, val) => {
+  return () => {
+    return new Promise((res, rej) => setTimeout(resolve ? res : rej, delay, val))
+  }
+};
+const tasks = [
+  taskFactorySample(50, true, 1),
+  taskFactorySample(100, true, 2),
+  taskFactorySample(500, false, 'error'),
+  taskFactorySample(200, true, 4),
+  taskFactorySample(100, false, 'error'),
+  taskFactorySample(100, false, 'error')
+];
+let pool_size = 2;
+
+runBatches(tasks, pool_size).then(console.log);
+
+
+async function runBatches(tasks, pool_size) {
+  let results = await getBatches(tasks, pool_size);
+  return results;
+}
+
+
+
+
+async function getBatches(tasks, pool_size) {
+  let pendingTasks = tasks.length;
+  let results = [];
+  let outsideResolve;
+  async function startNewTask(r) {
+    pendingTasks -= 1;
+    if (r === 'error') {
+      results.push({ error: 'error' });
+    } else {
+      results.push({ value: r })
+    }
+    let task = tasks.shift();
+    if (task) {
+      task()
+        .then((v) => startNewTask(v))
+        .catch((v) => startNewTask(v))
+    }
+    if (pendingTasks === 0) {
+      console.log('finished');
+      outsideResolve();
+    }
+  }
+
+  for (let i = 0; i < pool_size; i++) {
+    let task = tasks.shift();
+    task()
+      .then((v) => startNewTask(v))
+      .catch((v) => startNewTask(v))
+  }
+
+  let waitToFinish = await new Promise((res, rej) => outsideResolve = res);
+
+  return results;
+}
+
+
+/*
 async function runBatches(tasks, pool_size) {
   let results = [];
   let noStartedTasks = [...tasks]
@@ -30,5 +93,5 @@ async function runBatches(tasks, pool_size) {
   return results;
 }
 
-
-module.exports = runBatches;
+*/
+//module.exports = runBatches;
