f209e2ce5e355a7314528cf66f4b44d055b05bc3
javier171188 <javier171188@hotmail.com>
2021-12-15 19:32:26 -0600

    In exercise 4, the elements are printed in order

--- FullCourseReview/algorithms/exercise4/exercise4.js
+++ FullCourseReview/algorithms/exercise4/exercise4.js
@@ -1,5 +1,30 @@
 'use strict'
+const largeArray = [1, 3, 5, 7, 9].concat(new Array(5));
+const smallArray = [0, 2, 4, 6, 8];
+const largeArraySize = largeArray.length;
 
+mergeArrays(largeArray, smallArray);
+
+function mergeArrays(largeArray, smallArray) {
+    let smallArraySize = smallArray.length;
+    let j = 0;
+    for (let i = 0; i < smallArraySize; i++) {
+        if (smallArray[i] < largeArray[j]) {
+            console.log(smallArray[i]);
+        } else {
+            while (largeArray[j] <= smallArray[i]) {
+                console.log(largeArray[j]);
+                j += 1;
+            }
+            console.log(smallArray[i]);
+        }
+    }
+
+}
+
+
+
+/*
 function mergeArrays(largeArray, smallArray) {
     const largeArraySize = largeArray.length;
     const smallArraySize = smallArray.length;
@@ -8,6 +33,6 @@ function mergeArrays(largeArray, smallArray) {
         largeArray[startIndex + i] = number;
     });
     largeArray.sort();
-}
+}*/
 
-module.exports = mergeArrays;
+//module.exports = mergeArrays;
