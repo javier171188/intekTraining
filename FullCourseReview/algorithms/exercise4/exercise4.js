3fe82446d2abb9b8680c5d3956e23e4c00c76bfd
javier171188 <javier171188@hotmail.com>
2021-12-16 12:15:42 -0600

    Exercise 4 seems to be corrected

--- FullCourseReview/algorithms/exercise4/exercise4.js
+++ FullCourseReview/algorithms/exercise4/exercise4.js
@@ -1,42 +1,27 @@
 'use strict'
-const largeArray = [1, 3, 5, 7, 9].concat(new Array(5));
-const smallArray = [0, 2, 4, 6, 8];
-const largeArraySize = largeArray.length;
 
-mergeArrays(largeArray, smallArray);
+function introduceElement(element, array, index) {
+    for (let i = array.length - 1; i > index; i--) {
+        array[i] = array[i - 1];
+    }
+    array[index] = element;
+}
 
 function mergeArrays(largeArray, smallArray) {
     let smallArraySize = smallArray.length;
     let j = 0;
     for (let i = 0; i < smallArraySize; i++) {
         if (smallArray[i] < largeArray[j]) {
-            console.log(smallArray[i]);
-            largeArray.slice(j + 1, largeArray.length - i - 1) = largeArray.slice(j, largeArray.length - i);
-            //https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
-            largeArray[j] = smallArray[i];
+            introduceElement(smallArray[i], largeArray, j);
             j += 1;
         } else {
             while (largeArray[j] <= smallArray[i]) {
-                console.log(largeArray[j]);
                 j += 1;
             }
-            console.log(smallArray[i]);
+            introduceElement(smallArray[i], largeArray, j);
         }
     }
-
+    return largeArray;
 }
 
-
-
-/*
-function mergeArrays(largeArray, smallArray) {
-    const largeArraySize = largeArray.length;
-    const smallArraySize = smallArray.length;
-    const startIndex = largeArraySize - smallArraySize;
-    smallArray.forEach((number, i) => {
-        largeArray[startIndex + i] = number;
-    });
-    largeArray.sort();
-}*/
-
-//module.exports = mergeArrays;
+module.exports = mergeArrays;
--- FullCourseReview/algorithms/exercise4/exercise4.unit.test.js
+++ FullCourseReview/algorithms/exercise4/exercise4.unit.test.js
@@ -19,6 +19,7 @@ test('Empty arrays', () => {
     expect(largeArray).toEqual([1, 3, 5, 7, 9]);
     expect(largeArraySize === largeArray.length).toBe(true);
 
+
     largeArray = [];
     smallArray = [];
     largeArraySize = largeArray.length;
@@ -34,5 +35,37 @@ test('Same array', () => {
     const largeArraySize = largeArray.length;
     mergeArrays(largeArray, smallArray);
     expect(largeArray).toEqual([1, 1, 3, 3, 5, 5, 7, 7, 9, 9]);
+    expect(largeArraySize === largeArray.length).toBe(true);
+})
+
+test('Embedded array', () => {
+    let largeArray = [1, 3, 5, 7, 9].concat(new Array(5));
+    let smallArray = [0, 12, 13, 14, 15];
+    let largeArraySize = largeArray.length;
+    mergeArrays(largeArray, smallArray);
+    expect(largeArray).toEqual([0, 1, 3, 5, 7, 9, 12, 13, 14, 15]);
+    expect(largeArraySize === largeArray.length).toBe(true);
+
+    largeArray = [0, 12, 13, 14, 15].concat(new Array(3));
+    smallArray = [1, 2, 3];
+    largeArraySize = largeArray.length;
+    mergeArrays(largeArray, smallArray);
+    expect(largeArray).toEqual([0, 1, 2, 3, 12, 13, 14, 15]);
+    expect(largeArraySize === largeArray.length).toBe(true);
+})
+
+test('Already ordered', () => {
+    let largeArray = [0, 1, 2, 3].concat(new Array(3));
+    let smallArray = [4, 5, 6];
+    let largeArraySize = largeArray.length;
+    mergeArrays(largeArray, smallArray);
+    expect(largeArray).toEqual([0, 1, 2, 3, 4, 5, 6]);
+    expect(largeArraySize === largeArray.length).toBe(true);
+
+    largeArray = [0, 1, 2, 3].concat(new Array(3));
+    smallArray = [-4, -3, -2];
+    largeArraySize = largeArray.length;
+    mergeArrays(largeArray, smallArray);
+    expect(largeArray).toEqual([-4, -3, -2, 0, 1, 2, 3]);
     expect(largeArraySize === largeArray.length).toBe(true);
 })
