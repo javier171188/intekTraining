ab686db8d90070f5cc4cab88902f50e8a2b3c198
javier171188 <javier171188@hotmail.com>
2021-12-17 19:10:30 -0600

    Exercise 11 seems to be corrected..

--- FullCourseReview/coreJavaScript/exercise11/exercise11.js
+++ FullCourseReview/coreJavaScript/exercise11/exercise11.js
@@ -1,98 +1,4 @@
 'use strict';
-let html =
-    `<section>
-    <div id="1" class="note"><input type="checkbox" class="is-complete" checked> </div>
-    <div id="2" class="note"></div>
-    <div id="3" class="note"><input type="checkbox" class="is-complete" checked></div>
-    <div id="4" class="note"></div>
-    <div id="5" class="note"><input type="checkbox" class="is-complete" checked></div>
-    <div id="6" class="note"><input type="checkbox" class="is-complete" ></div>
-    <div class="otherclass"></div>
-    <div></div>
-    </section>`;
-
-//let html = '<div id="1" class="note"><input type="checkbox" class="is-complete" checked> </div>';
-
-let selector = "div.note < input.is-complete[checked]";
-let result = querySelectorAll(selector);
-console.log(result);
-
-
-function simpleSelector(selector, htmlStr) {
-    let [elementType, elementClass] = selector.split('.');
-
-    //let propertyPattern = new RegExp(/\[.*?\]/, 'g');
-    let fullPattern = new RegExp(`<${elementType}.*?\/${elementType}>`, 'gs');
-    let openingPattern = new RegExp(`<${elementType}.*?/?>`, 'g');
-
-    let property = '';
-    if (elementClass) {
-        /*var propertyIt = elementClass.matchAll(propertyPattern);
-        let propertyLi = [...propertyIt];
-        propertyLi = propertyLi.map(p => p[0]);
-
-        console.log(propertyLi);*/
-
-        let openingIndex = [];
-        let closingIndex = [];
-        for (let i in elementClass) {
-            if (elementClass[i] === '[') {
-                openingIndex.push(i);
-            }
-            else if (elementClass[i] === ']') {
-                closingIndex.push(i);
-            }
-        }
-
-        let propertyLi = [];
-        for (let i in openingIndex) {
-            propertyLi.push(elementClass.slice(openingIndex[i], closingIndex[i] + 1));
-        }
-
-
-        if (propertyLi.length > 0) {
-            property = propertyLi[0];
-            elementClass = elementClass.replace(property, '');
-            property = property.slice(1, -1);
-        }
-    }
-
-    let elementsIt = htmlStr.matchAll(fullPattern);
-    let openingIt = htmlStr.matchAll(openingPattern);
-
-
-    var elementsList = [];
-    if (elementsIt) {
-        elementsList = [...elementsIt].map(e => e[0]);
-        let openingList = [...openingIt].map(e => e[0]);
-        if (elementsList.length < openingList.length) {
-            elementsList = openingList;
-        }
-    }
-
-    if (elementClass) {
-        elementsList = elementsList.filter(e => {
-            let openElement = e.matchAll(openingPattern);
-            openElement = [...openElement].map(e => e[0])[0];
-
-            let hasClass = openElement.search(`class="${elementClass}"`);
-
-            return hasClass > -1;
-        })
-    }
-
-    if (property) {
-        elementsList = elementsList.filter(e => {
-            let openElement = e.matchAll(openingPattern);
-            openElement = [...openElement].map(e => e[0])[0];
-            let hasProperty = openElement.search(` ${property}`);
-
-            return hasProperty > -1;
-        })
-    }
-
-    return elementsList;
-}
 
 function querySelectorAll(selector) {
     if (!selector) {
@@ -103,16 +9,15 @@ function querySelectorAll(selector) {
     }
     const [parentSelector, childrenSelector] = selector.split('<');
 
-    var parentsList = simpleSelector(parentSelector, html);
+    let parents = document.querySelectorAll(parentSelector);
+    if (!childrenSelector) return [...parents];
 
-    if (childrenSelector) {
-        parentsList = parentsList.filter(p => {
-            let children = simpleSelector(childrenSelector, p);
-            return children.length > 0;
-        })
-    }
+    parents = [...parents].filter((p) => {
+        let children = p.querySelectorAll(childrenSelector);
+        return children.length > 0;
+    })
 
-    return parentsList;
+    return parents;
 }
 
-//module.exports = { querySelectorAll };
+module.exports = { querySelectorAll };
--- FullCourseReview/coreJavaScript/exercise11/exercise11.unit.test.js
+++ FullCourseReview/coreJavaScript/exercise11/exercise11.unit.test.js
@@ -1,57 +1,54 @@
-'use strict';
+/**
+ * @jest-environment jsdom
+ */
 const { querySelectorAll } = require('./exercise11');
 
+document.body.innerHTML = `<section>
+<div id="1" class="note"><input type="checkbox" class="is-complete" checked> </div>
+<div id="2" class="note"></div>
+<div id="3" class="note"><input type="checkbox" class="is-complete" checked></div>
+<div id="4" class="note"></div>
+<div id="5" class="note"><input type="checkbox" class="is-complete" checked></div>
+<div id="6" class="note"><input type="checkbox" class="is-complete" ></div>
+<div class="anotherclass"></div>
+<div></div>
+</section>`
+
+let mainSection = [...document.querySelectorAll('section')];
+let divs = [...mainSection[0].querySelectorAll('div')];
 
 test('Simple test', () => {
     let parents = querySelectorAll("div.note < input.is-complete[checked]");
     expect(parents).toEqual([
-        '<div id="1" class="note"><input type="checkbox" class="is-complete" checked> </div>',
-        '<div id="3" class="note"><input type="checkbox" class="is-complete" checked></div>',
-        '<div id="5" class="note"><input type="checkbox" class="is-complete" checked></div>'
+        divs[0], divs[2], divs[4]
     ])
 })
 
+
 test('Basic selectors', () => {
     let elements = querySelectorAll("div");
-    expect(elements).toEqual([
-        '<div id="1" class="note"><input type="checkbox" class="is-complete" checked> </div>',
-        '<div id="2" class="note"></div>',
-        '<div id="3" class="note"><input type="checkbox" class="is-complete" checked></div>',
-        '<div id="4" class="note"></div>',
-        '<div id="5" class="note"><input type="checkbox" class="is-complete" checked></div>',
-        '<div id="6" class="note"><input type="checkbox" class="is-complete" ></div>',
-        '<div class="otherclass"></div>',
-        '<div></div>'
-    ]);
+    expect(elements).toEqual(divs);
 
     let section = querySelectorAll('section');
-    section = section.map(s => s.replace(/\s/g, ''))//<----Avoid Regex
-    expect(section).toEqual([
-        `<section>
-            <div id="1" class="note"><input type="checkbox" class="is-complete" checked> </div>
-            <div id="2" class="note"></div>
-            <div id="3" class="note"><input type="checkbox" class="is-complete" checked></div>
-            <div id="4" class="note"></div>
-            <div id="5" class="note"><input type="checkbox" class="is-complete" checked></div>
-            <div id="6" class="note"><input type="checkbox" class="is-complete" ></div>
-            <div class="otherclass"></div>
-            <div></div>
-            </section>`.replace(/\s/g, '')//<----Avoid Regex
-    ])
+    expect(section).toStrictEqual(mainSection)
 
     let empty = querySelectorAll('');
     expect(empty).toEqual([]);
 
-    let other = querySelectorAll('div.otherclass');
-    expect(other).toEqual(['<div class="otherclass"></div>'])
+    let other = querySelectorAll('div.anotherclass');
+    expect(other).toEqual([divs[6]])
 })
+
+
 
 test('No checked', () => {
     let parents = querySelectorAll("div.note < input.is-complete");
     expect(parents).toEqual([
-        '<div id="1" class="note"><input type="checkbox" class="is-complete" checked> </div>',
-        '<div id="3" class="note"><input type="checkbox" class="is-complete" checked></div>',
-        '<div id="5" class="note"><input type="checkbox" class="is-complete" checked></div>',
-        '<div id="6" class="note"><input type="checkbox" class="is-complete" ></div>'
+        divs[0], divs[2], divs[4], divs[5]
     ])
 })
+
+test('No match', () => {
+    let parents = querySelectorAll("div.note < input.anotherclass");
+    expect(parents).toEqual([])
+}) 
