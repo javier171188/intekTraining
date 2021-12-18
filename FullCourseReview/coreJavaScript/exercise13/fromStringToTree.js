55a9662ba30823435d5a6a4946654aa31f9ba31a
javier171188 <javier171188@hotmail.com>
2021-12-17 19:46:19 -0600

    Working on transform from object to string

---
+++ FullCourseReview/coreJavaScript/exercise13/fromStringToTree.js
@@ -0,0 +1,21 @@
+let root = {
+    value: 1,
+    left: {
+        value: 2,
+        left: {
+            value: 3,
+            left: null,
+            right: null
+        },
+        right: {
+            value: 4,
+            left: {
+                value: 5,
+                left: {},
+                right: {}
+            },
+            right: {}
+        }
+    },
+    right: {}
+}
