88c0e661db0e42df6d94f3c94dc874a8626e83fd
javier171188 <javier171188@hotmail.com>
2021-12-20 19:46:24 -0600

    Working on exercise 19

--- FullCourseReview/coreJavaScript/exercise19/nb-test.js
+++ FullCourseReview/coreJavaScript/exercise19/nb-test.js
@@ -1,4 +1,34 @@
 (function (global) {
+    //let root = document.getElementById('results');
+    const result = function (text, pass) {
+        const el = document.createElement('li');
+        el.textContent = text;
+        pass !== undefined && (el.style.color = pass ? 'green' : 'red');
+        return el;
+    }
+    let assert = function (pass, message) {
+        let root = document.getElementById('results');
+        return root.appendChild(result(message, pass))
+    };
+    global.assert = assert;
+
+    function test(description, testBlock) {
+        let saving = assert;
+        let parent = assert(undefined, description)
+            .appendChild(document.createElement('ul'));
+        (function (parent) {
+            global.assert = function (pass, message) {
+                return parent.appendChild(result(message, pass))
+            }
+            testBlock();
+        })(parent)
+        global.assert = saving;
+    }
+    global.test = test;
+})(window);
+
+
+/*(function (global) {
     const result = (text, pass) => {
         const el = document.createElement('li');
         el.textContent = text;
@@ -39,4 +69,4 @@
         global.assert = saving;
     }
     global.test = test;
-})(window);
+})(window);*/
