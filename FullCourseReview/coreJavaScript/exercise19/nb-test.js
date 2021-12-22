b22d2a1f15c93d92d558dda0df2d40270cef6160
javier171188 <javier171188@hotmail.com>
2021-12-22 12:39:02 -0600

    Exercise 19 get the nodes

--- FullCourseReview/coreJavaScript/exercise19/nb-test.js
+++ FullCourseReview/coreJavaScript/exercise19/nb-test.js
@@ -1,3 +1,4 @@
+// Works with the second setTimeout
 /*(function (global) {
     let parent = document.getElementById('results');
     const result = function (text, pass) {
@@ -7,7 +8,6 @@
         return el;
     }
     let assert = function (pass, message) {
-        console.log(parent);
         return parent.appendChild(result(message, pass))
     };
     global.assert = assert;
@@ -17,22 +17,87 @@
         let parent = assert(undefined, description)
             .appendChild(document.createElement('ul'));
 
-
-        (function (parent) {
-            global.assert = function (pass, message) {
-                console.log('inside', parent)
-                return parent.appendChild(result(message, pass))
-            }
-            testBlock();
-        })(parent)
+        global.assert = function (pass, message) {
+            return parent.appendChild(result(message, pass))
+        }
+        testBlock();
 
         global.assert = saving;
     }
     global.test = test;
 })(window);*/
 
-// Works without arrow functions
 
+(function (global) {
+    let parent = document.getElementById('results');
+    const result = function (text, pass) {
+        const el = document.createElement('li');
+        el.textContent = text;
+        pass !== undefined && (el.style.color = pass ? 'green' : 'red');
+        return el;
+    }
+    let assert = function (pass, message) {
+        return parent.appendChild(result(message, pass))
+    };
+    global.assert = assert;
+
+    function test(description, testBlock) {
+        let saving = assert;
+        let parent = assert(undefined, description)
+            .appendChild(document.createElement('ul'));
+
+        global.assert = function (pass, message) {
+            return parent.appendChild(result(message, pass))
+        }
+        let originalSetTimeout = global.setTimeout;
+
+        function setTimeout(...params) {
+            console.log(params);
+            console.log(parent);
+            setTimeout(assert, 500, true, "test delayed B");
+        }
+        global.setTimeout = setTimeout;
+        testBlock();
+
+        global.assert = saving;
+        global.setTimeout = originalSetTimeout
+
+    }
+    global.test = test;
+})(window);
+
+/*
+function setFunctions(global) {
+    let root = document.getElementById('results');
+    function result(text, pass) {
+        const el = document.createElement('li');
+        el.textContent = text;
+        pass !== undefined && (el.style.color = pass ? 'green' : 'red');
+        return el;
+    }
+    function assert(pass, message) {
+        return root.appendChild(result(message, pass))
+    };
+    global.assert = assert;
+
+    let originalSetTimeout = global.setTimeout;
+    function setTimeout(fn, delay, ...params) {
+
+        originalSetTimeout(fn, delay, ...params)
+    }
+    global.setTimeout = setTimeout;
+
+    function test(description, testBlock) {
+        let node = root.appendChild(document.createElement('ul'));
+        node.textContent = description;
+
+        testBlock();
+    }
+    global.test = test;
+}
+setFunctions(window);
+*/
+/*
 (function (global) {
     let root = document.getElementById('results');
 
@@ -46,18 +111,18 @@
     let originalSetTimeout = global.setTimeout;
 
     let assert = function (pass, message) {
-        let root = document.getElementById('results');
+        //let root = document.getElementById('results');
         return root.appendChild(result(message, pass))
     };
 
     let setTimeout = function (fn, delay, ...params) {
         global.assert = function (pass, message) { //root.appendChild(result(message, pass)) 
             let node = global.node;
-
-            node.appendChild(result(message, pass));
             console.log(node);
+            node.appendChild(result(message, pass));
+
         };
-        originalSetTimeout(() => { fn(...params) }, delay)
+        originalSetTimeout(fn, delay, ...params)
     }
 
     global.setTimeout = setTimeout;
@@ -83,7 +148,7 @@
         //global.assert = saving;
     }
     global.test = test;
-})(window);
+})(window);*/
 
 
 /*(function (global) {
