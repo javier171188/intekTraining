89c85f40534876c32fa52fab260ad025ec3a9a27
javier171188 <javier171188@hotmail.com>
2021-12-22 19:45:35 -0600

    Working on the graph and fix storybook

--- FullCourseReview/reactJS/exercise41/chart/src/stories/Chart.jsx
+++ FullCourseReview/reactJS/exercise41/chart/src/stories/Chart.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+//import ReactDOM from 'react-dom';
+//1import * as V from 'victory';
+import { VictoryChart, VictoryLine } from 'victory';
+//https://formidable.com/open-source/victory/docs
+
 
 export default function Chart({ task: { id, title, state }, onArchiveTask, onPinTask }) {
     return (
-        <div className="list-item">
-            Hello :D
-        </div>
+        <VictoryChart
+            theme={VictoryTheme.material}
+        >
+            <VictoryLine
+                style={{
+                    data: { stroke: "#c43a31" },
+                    parent: { border: "1px solid #ccc" }
+                }}
+                data={[
+                    { x: 1, y: 2 },
+                    { x: 2, y: 3 },
+                    { x: 3, y: 5 },
+                    { x: 4, y: 4 },
+                    { x: 5, y: 7 }
+                ]}
+            />
+        </VictoryChart>
     );
 }
