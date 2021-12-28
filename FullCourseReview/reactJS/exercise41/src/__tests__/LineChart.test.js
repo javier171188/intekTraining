1f209c6f6fe4383bcd963820a6f4299084a25775
javier171188 <javier171188@hotmail.com>
2021-12-28 11:37:11 -0600

    Reducers tested

--- FullCourseReview/reactJS/exercise41/src/__tests__/LineChart.test.js
+++ FullCourseReview/reactJS/exercise41/src/__tests__/LineChart.test.js
@@ -5,6 +5,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ProviderMock from '../__mocks__/ProviderMock';
 import { getDataAction } from '../redux/actions';
+import reducers from '../redux/reducers';
 import LineChart from '../LineChart';
 
 const chart = shallow(
@@ -24,6 +25,21 @@ test("Check the action", () => {
         type: "GET_NEW_DATA",
         payload
     }
-
     expect(getDataAction()).toEqual(expected);
+})
+
+describe('Check the reducers', () => {
+    let state = { x: 1, y: 1 };
+    test("Return the default", () => {
+        expect(reducers(state, 'ANOTHER_ACTION')).toEqual(state);
+    })
+
+    test("Apply SET_NEW_DATA", () => {
+        let newState = { data: { x: 1, y: 1 }, x: 1, y: 1 }
+        let action = {
+            type: 'SET_NEW_DATA',
+            payload: { x: 1, y: 1 }
+        }
+        expect(reducers(state, action)).toEqual(newState);
+    })
 })
