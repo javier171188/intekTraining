878657357b33c7333bda00ab567fdddd04853b63
javier171188 <javier171188@hotmail.com>
2021-12-28 19:45:35 -0600

    Working on the gallery of exercise 42

---
+++ FullCourseReview/reactJS/exercise42/src/elements/Header.jsx
@@ -0,0 +1,7 @@
+import Box from '@mui/material/Box';
+
+const Header = () => {
+    return <Box component="h1" color="brown">Gallery</Box>
+}
+
+export default Header;
--- FullCourseReview/reactJS/exercise42/src/index.js
+++ FullCourseReview/reactJS/exercise42/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Header from "./elements/Header";
 
 const App = () => {
-    return <h1> Gallery </h1>
+    return <Header />
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
