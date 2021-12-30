155e1b8e136bc005c75c851dcd16f26ba30f3e6d
javier171188 <javier171188@hotmail.com>
2021-12-30 12:23:56 -0600

    Double images are shown

--- FullCourseReview/reactJS/exercise42/src/components/IndividualPhoto.jsx
+++ FullCourseReview/reactJS/exercise42/src/components/IndividualPhoto.jsx
@@ -11,32 +11,47 @@ const IndividualPhoto = (props) => {
 
     let boxWidth = "30vw";
     let boxHeight = "30vw";
-    if (width === height * 2)
-        return <Box sx={{
-            width: boxWidth,
-            height: boxHeight,
-            border: "solid",
-            margin: 5,
-            float: "left",
-            alignItems: "center"
-        }} >
-            <Image
-                src={src}
-                imageStyle={{
-                    width: widthStr,
-                    height: heightStr,
-                    left: '50%',
-                    top: '50%',
-                    transform: 'translate(-50%, -50%)'
-                }}
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-
-                }}
-            />
-        </Box>
+    let percentageToPlace = "50%";
+    let paddingTop = '100%';
+    if (width === height * 2) {
+        boxWidth = "60vw";
+        widthStr = "60vw";
+        heightStr = "30vw";
+        percentageToPlace = '0';
+        paddingTop = '0%';
+    }
+    else if (2 * width === height) {
+        boxHeight = "60vw";
+        widthStr = "30vw";
+        heightStr = "60vw";
+        percentageToPlace = '0';
+    }
+    return <Box sx={{
+        width: boxWidth,
+        height: boxHeight,
+        border: "solid",
+        margin: 5,
+        float: "left",
+        alignItems: "center",
+        paddingTop: 0
+    }} >
+        <Image
+            src={src}
+            imageStyle={{
+                width: widthStr,
+                height: heightStr,
+                left: percentageToPlace,
+                top: percentageToPlace,
+                transform: `translate(-${percentageToPlace}, -${percentageToPlace})`
+            }}
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                paddingTop
+            }}
+        />
+    </Box>
 }
 
 export default IndividualPhoto;
