09245474a426d430205ff96e1f2447a2a60684f6
javier171188 <javier171188@hotmail.com>
2021-12-30 19:47:36 -0600

    Working on the tests

--- FullCourseReview/reactJS/exercise42/src/__test__/exercise42.test.js
+++ FullCourseReview/reactJS/exercise42/src/__test__/exercise42.test.js
@@ -6,9 +6,21 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Gallery from '../components/Gallery';
 
+function wait(time) {
+    return new Promise((res, rej) => {
+
+    }, time)
+}
+
 describe('<Gallery />', () => {
+    const gallery = mount(<Gallery />);
     test('Render the component', () => {
-        const gallery = mount(<Gallery />);
         expect(gallery.length).toEqual(1);
+    });
+
+    test('Check the initial state', () => {
+        expect(gallery.find('img').length).toEqual(0);
     })
+
+
 })
