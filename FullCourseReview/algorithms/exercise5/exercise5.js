f566f4b05c70fa20c7bf3a378820e6c55a1b97a8
javier171188 <javier171188@hotmail.com>
2022-01-07 16:35:41 -0600

    Exercise 5 seems to be corrected

--- FullCourseReview/algorithms/exercise5/exercise5.js
+++ FullCourseReview/algorithms/exercise5/exercise5.js
@@ -4,11 +4,11 @@ function reverseBlocks(arr, blockSize) {
     if (blockSize < 1) {
         blockSize = 1;
     }
-    for (let i = 0; i < arr.length; i += blockSize) {
-        let currentBlock = arr.slice(i, i + blockSize);
-        currentBlock.reverse();
-        for (let j = 0; j < currentBlock.length; j++) {
-            arr[i + j] = currentBlock[j]
+    let arraySize = arr.length;
+    for (let i = 0; i < arraySize; i += blockSize) {
+        let currentSize = Math.min(blockSize, arr.length - i)
+        for (let j = 0; j < currentSize / 2; j++) {
+            [arr[i + j], arr[i + currentSize - j - 1]] = [arr[i + currentSize - j - 1], arr[i + j]];
         }
     }
     return arr
