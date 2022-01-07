a3968eeee5571d01a4032482e89a07eeb9cdea0d
javier171188 <javier171188@hotmail.com>
2022-01-07 13:08:54 -0600

    Exercise 1 seems to be corrected

--- FullCourseReview/promises/exercise1/exercise1.js
+++ FullCourseReview/promises/exercise1/exercise1.js
@@ -4,6 +4,10 @@ require('regenerator-runtime/runtime');
 async function runBatches(tasks, pool_size) {
   if (tasks.length < 1) return [];
   let startingTasks = [...tasks]
+
+  if (pool_size > tasks.length || pool_size < 1) {
+    pool_size = tasks.length;
+  }
   let results = await getBatches(startingTasks, pool_size);
   return results;
 }
@@ -13,23 +17,20 @@ async function getBatches(tasks, pool_size) {
   let results = [];
   let outsideResolve;
 
-  //This check could be in the runBatches function
-  if (pool_size > tasks.length || pool_size < 1) {
-    pool_size = tasks.length;
-  }
+  tasks = tasks.map((fn, position) => ({ fn, position }))
 
-  async function startNewTask(r) {
+  async function startNewTask(r, position) {
     pendingTasks -= 1;
     if (r === 'error') {
-      results.push({ error: 'error' });
+      results.push({ error: 'error', position });
     } else {
-      results.push({ value: r })
+      results.push({ value: r, position })
     }
     let task = tasks.shift();
     if (task) {
-      task()
-        .then((v) => startNewTask(v))
-        .catch((v) => startNewTask(v))
+      task.fn()
+        .then((v) => startNewTask(v, task.position))
+        .catch((v) => startNewTask(v, task.position))
     }
     if (pendingTasks === 0) {
       console.log('finished');
@@ -39,13 +40,18 @@ async function getBatches(tasks, pool_size) {
 
   for (let i = 0; i < pool_size; i++) {
     let task = tasks.shift();
-    task()
-      .then((v) => startNewTask(v))
-      .catch((v) => startNewTask(v))
+    task.fn()
+      .then((v) => startNewTask(v, task.position))
+      .catch((v) => startNewTask(v, task.position))
   }
 
   await new Promise((res, rej) => outsideResolve = res);
 
+  results.sort((a, b) => {
+    return a.position - b.position
+  }).map(r => {
+    return delete r.position
+  })
   return results;
 }
 
