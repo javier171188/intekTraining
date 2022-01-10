c5707c343e62e4ad7512389f5f684aa53afd9727
javier171188 <javier171188@hotmail.com>
2022-01-10 14:56:00 -0600

    Test set

--- FullCourseReview/coreJavaScript/exercise11/exercise11.js
+++ FullCourseReview/coreJavaScript/exercise11/exercise11.js
@@ -12,9 +12,17 @@ function querySelectorAll(selector) {
     let parents = document.querySelectorAll(parentSelector);
     if (!childrenSelector) return [...parents];
 
+    let children = document.querySelectorAll(childrenSelector);
+
     parents = [...parents].filter((p) => {
-        let children = p.querySelectorAll(childrenSelector);
-        return children.length > 0;
+        //let children = p.querySelectorAll(childrenSelector);
+        //return children.length > 0;
+
+        for (let child of children) {
+            if (child === p) return true;
+        }
+
+        return false;
     })
 
     return parents;
