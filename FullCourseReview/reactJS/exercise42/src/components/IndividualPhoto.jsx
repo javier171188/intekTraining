b16502c01c716da6df071c473db66f80204c7874
javier171188 <javier171188@hotmail.com>
2022-01-14 16:47:40 -0600

    Revert "Class element implemented"
    This reverts commit e128b7d6262df82f3a7c9bd8d200adbc08fbda67.

--- FullCourseReview/reactJS/exercise42/src/components/IndividualPhoto.jsx
+++ FullCourseReview/reactJS/exercise42/src/components/IndividualPhoto.jsx
@@ -1,86 +1,10 @@
 'use strict';
-import React, { Component } from 'react';
+import React from 'react';
 import Image from 'material-ui-image';
 import { Box, Container } from '@material-ui/core';
 //https://codepen.io/iamsaief/pen/jObaoKo?editors=0100
 
 
-class IndividualPhoto extends Component {
-    constructor(givenProps) {
-        super();
-        this.src = givenProps.src;
-        this.width = givenProps.width;
-        this.height = givenProps.height;
-        this.borderColor = givenProps.borderColor;
-
-        this.widthStr = '100%';
-        this.heightStr = 'auto';
-        this.percentageToPlaceV = "50%";
-        this.percentageToPlaceH = "50%";
-        this.paddingTop = '100%';
-        this.containerClass = 'normal';
-        this.gridColumn, gridRow;
-        if (this.width < this.height) [this.widthStr, this.heightStr] = [this.heightStr, this.widthStr];
-
-        if (this.width === this.height * 2) {
-            this.widthStr = "64vw";
-            this.heightStr = "auto";
-            this.percentageToPlaceH = '0';
-            this.percentageToPlaceV = '0';
-            this.paddingTop = '0%';
-            this.gridColumn = "span 2";
-            this.containerClass = "wide";
-            this.gridColumn = "span 2";
-        }
-        else if (2 * this.width === this.height) {
-            this.widthStr = "auto";
-            this.heightStr = "63.6vw";
-            this.percentageToPlaceH = '0';
-            this.percentageToPlaceV = '0';
-            this.gridRow = "span 2";
-            this.containerClass = "tall";
-            this.gridRow = "span 2";
-        }
-
-    }
-
-    render() {
-        return <Box
-            className={this.containerClass}
-            sx={{
-                border: "solid",
-                borderWidth: "0.3vw",
-                gridColumn: this.gridColumn,
-                gridRow: this.gridRow
-            }}   >
-
-            {<Image
-                src={this.src}
-                imageStyle={{
-                    width: this.widthStr,
-                    height: this.heightStr,
-                    maxWidth: "100%",
-                    verticalAlign: "middle",
-                    display: "inline-block",
-                    position: "absolute",
-                    left: this.percentageToPlaceH,
-                    top: this.percentageToPlaceV,
-                    transform: `translate(-${this.percentageToPlaceH}, -${this.percentageToPlaceV})`
-                }}
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    justify: "center",
-                    paddingTop: this.paddingTop,
-                    height: "6%"
-                }}
-            />}
-        </Box >
-    }
-}
-
-/*
 const IndividualPhoto = (props) => {
     let { src, width, height, borderColor } = props;
     let widthStr = '100%';
@@ -91,6 +15,9 @@ const IndividualPhoto = (props) => {
     let containerClass = 'normal';
     let gridColumn, gridRow;
     if (width < height) [widthStr, heightStr] = [heightStr, widthStr];
+
+
+
 
     if (width === height * 2) {
         widthStr = "64vw";
@@ -143,6 +70,6 @@ const IndividualPhoto = (props) => {
             }}
         />}
     </Box >
-}*/
+}
 
 export default IndividualPhoto;
