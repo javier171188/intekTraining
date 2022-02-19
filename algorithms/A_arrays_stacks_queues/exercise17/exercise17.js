f33cc74c548852ae2ca5275c60d38880b9212ad1
javier171188 <javier171188@hotmail.com>
2022-02-18 18:42:30 -0600

    Working on exercise 17

---
+++ algorithms/A_arrays_stacks_queues/exercise17/exercise17.js
@@ -0,0 +1,14 @@
+'use strict';
+//https://www.geeksforgeeks.org/efficient-method-to-store-a-lower-triangular-matrix-using-row-major-mapping/
+
+class triangularArray {
+    constructor(rows, columns) {
+        //TODO: set the correct dimension
+        this.elements = new Array(rows * columns);
+    }
+
+    set(row, colum) {
+
+    }
+
+}
